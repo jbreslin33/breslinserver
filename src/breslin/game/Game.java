23691702bac3d1508e0b92fdf0319f926f9431d0
jbreslin33 <jbreslin33@localhost>
2011-07-20 02:45:10 -0400

    got a headless listen server. atleast one that listens. does not process anthing yet though.

--- src/breslin/game/Game.java
+++ src/breslin/game/Game.java
@@ -6,13 +6,22 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.math.ColorRGBA;
+
+
+//networking
+import java.io.*;
+
+//for headless server
+import com.jme3.system.JmeContext;
+import com.jme3.system.JmeContext.Type;
+
 /** Sample 1 - how to get started with the most simple JME 3 application.
  * Display a blue 3D cube and view from all sides by
  * moving the mouse and pressing the WASD keys. */
-public class HelloJME3 extends SimpleApplication {
-    public static void main(String[] args){
-        HelloJME3 app = new HelloJME3();
-        app.start();
+public class Game extends SimpleApplication {
+    public static void main(String[] args) {
+        Game app = new Game();
+        app.start(JmeContext.Type.Headless);
     }
     @Override
     public void simpleInitApp() {
@@ -22,5 +31,13 @@ public class HelloJME3 extends SimpleApplication {
         mat.setColor("Color", ColorRGBA.Blue);
         geom.setMaterial(mat);
         rootNode.attachChild(geom);
+
+        //start networking...
+        try {
+        	new GameServerThread().start();
+		} catch (IOException e) {
+		    e.printStackTrace();
+        }
+
     }
 }
