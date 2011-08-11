4aef685ab957e505299ad893dde6f216d6d3044d
jbreslin33 <jbreslin33@localhost>
2011-08-11 12:07:54 -0400

    fixin

--- compile_3.bat
+++ compile_3.bat
@@ -1,3 +1,2 @@
 mkdir build
-javac src/breslin/game/message/Message.java
-javac -d build -cp "lib/jME3-lwjgl-natives.jar;lib/jMonkeyEngine3.jar;lib/lwjgl.jar;." src/breslin/game/*.java
+javac -d build -cp "lib/jME3-lwjgl-natives.jar;lib/jMonkeyEngine3.jar;lib/lwjgl.jar;." src/breslin/game/Game.java
