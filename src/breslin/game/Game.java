d3f844497f1417480c5508afe98ebfa0db11b53a
jbreslin33 <jbreslin33@localhost>
2011-08-11 12:09:33 -0400

    changed source this needs to be copied....

--- src/breslin/game/Game.java
+++ src/breslin/game/Game.java
@@ -1,63 +1,129 @@
 package breslin.game;
 
+//game
 import com.jme3.app.SimpleApplication;
+
+//graphics
 import com.jme3.material.Material;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.math.ColorRGBA;
 
+//input
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.KeyInput;
 
 //networking
+import java.net.*;
 import java.io.*;
 
-//for headless server
-import com.jme3.system.JmeContext;
-import com.jme3.system.JmeContext.Type;
 
-/** Sample 1 - how to get started with the most simple JME 3 application.
- * Display a blue 3D cube and view from all sides by
- * moving the mouse and pressing the WASD keys. */
-public class Game extends SimpleApplication {
-    public static void main(String[] args) {
-        Game app = new Game();
-        app.start(JmeContext.Type.Headless);
-    }
-    @Override
-    public void simpleInitApp()
-    {
-		//this is all I want server to do for now
-		startNetworking();
+public class Game extends SimpleApplication
+{
 
-    }
+	boolean isRunning     = false;
+	boolean mSentJoinGame = false;
+	String  mServerIP;
 
-	void gameLoop()
+	public static void main(String[] args)
 	{
+        Game app = new Game(args[0]);
+        app.start();
+    }
 
-
+	//constructor
+	Game(String serverIP)
+	{
+		mServerIP = serverIP;
 	}
 
-	void startNetworking()
+    public void simpleInitApp()
+    {
+		initializeKeys();
+		isRunning = true;
+    }
+
+	/** Custom Keybinding: Map named actions to inputs. */
+  	private void initializeKeys()
+  	{
+	   	// You can map one or several inputs to one named action
+	   	inputManager.addMapping("Join",   new KeyTrigger(KeyInput.KEY_J));
+	   	inputManager.addMapping("Quit",   new KeyTrigger(KeyInput.KEY_Q));
+
+    	// Add the names to the action listener.
+    	inputManager.addListener(analogListener, new String[]{"Join", "Quit"});
+	}
+
+
+	void joinGame()
 	{
-        //start networking...
-        try
-        {
-        	new GameServerThread().start();
+
+		mSentJoinGame = true;
+		int messageCount = 0;
+
+    	//networking
+    	try
+    	{
+
+    		// get a datagram socket
+			DatagramSocket socket = new DatagramSocket();
+
+			// send request
+    		byte[] buf = new byte[1400];
+
+			//write to message
+    		buf[0] = -101;
+    		buf[1] = 'n';
+
+			messageCount = 5;
+
+			//now make new buffer so that it's only as big as needed.
+			byte[] realByteArray = new byte[messageCount];
+
+			for (int i = 0; i < messageCount; i++)
+			{
+				realByteArray[i] = buf[i];
+			}
+
+			//create DataGramPacket
+    		InetAddress address = InetAddress.getByName(mServerIP);
+			DatagramPacket packet = new DatagramPacket(realByteArray, realByteArray.length, address, 30004);
+
+			//send packet to server
+    		socket.send(packet);
+
+			//print out what you sent for debugging
+			String s = new String(packet.getData());
+
+    		System.out.println("Data Sent:" + s);
+
 		}
 		catch (IOException e)
 		{
-		    e.printStackTrace();
-        }
+			e.printStackTrace();
+    	}
 	}
 
 
-	void createShape()
-	{
-		Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-		Geometry geom = new Geometry("Box", b);
-		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		mat.setColor("Color", ColorRGBA.Blue);
-		geom.setMaterial(mat);
-        rootNode.attachChild(geom);
-	}
+	private AnalogListener analogListener = new AnalogListener()
+  	{
+  		public void onAnalog(String name, float value, float tpf)
+  		{
+      		if (isRunning)
+      		{
+        		if (name.equals("Join"))
+        		{
+					if (!mSentJoinGame)
+					{
+						System.out.println("hetttt");
+        				joinGame();
+					}
+        		}
+
+      		}
+		}
+
+	};
 }
