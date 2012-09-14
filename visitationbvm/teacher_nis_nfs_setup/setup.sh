d31b7f16d5f07a0670dba28865e93a250120e738
jbreslin33 <jbreslin33@localhost>
2012-09-14 12:47:02 -0400

    fixed up teacher script

--- visitationbvm/teacher_nis_nfs_setup/setup.sh
+++ visitationbvm/teacher_nis_nfs_setup/setup.sh
@@ -1,3 +1,5 @@
+echo update dpkg 
+sudo dpkg --configure -a
 echo install ssh
 sudo apt-get install ssh
 echo update system
@@ -6,25 +8,41 @@ echo install portmap
 sudo apt-get install portmap
 echo update defaults
 sudo update-rc.d portmap defaults 10
+
 echo install nis
 sudo apt-get install nis
+
 echo update hosts file
 cp hosts /etc
+
 echo update yp.conf file
 cp yp.conf /etc
+
 echo update nsswitch.conf file
 cp nsswitch.conf /etc
+
 echo update lightdm.conf file
 cp lightdm.conf /etc/lightdm
+
 echo update common-session file
 cp common-session /etc/pam.d
 echo change permissions of home
 sudo chmod 777 /home
+
+echo cron jobs
+sudo cp crontab /etc
+sudo mkdir /scripts
+sudo cp mount_nfs.sh /scripts 
+sudo chmod 777 /mnt
+
 echo install nfs-common
 sudo apt-get install nfs-common
+
 sudo cp fstab /etc
+
 echo remove unity
 sudo apt-get remove unity
+
 echo make vm directory
 sudo mkdir /vm
 sudo chmod -R 777 /vm
@@ -33,14 +51,13 @@ sudo mkdir /vm/xp
 sudo chmod -R 777 /vm/xp
 echo install virtualbox
 sudo apt-get install virtualbox
-echo install clusterssh
-sudo apt-get install clusterssh
+
 echo install vlc player
 sudo apt-get install vlc
+
 echo for teacher admin of epoptes
 sudo apt-get install epoptes
 echo add student to list epoptes group
 sudo gpasswd -a student epoptes
-
 
 echo complete you can now reboot and login 
