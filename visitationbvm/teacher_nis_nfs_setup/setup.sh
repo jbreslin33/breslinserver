904ce1c12bf2a096f329a939712e1f5d2ffd0b19
jbreslin33 <jbreslin33@localhost>
2012-09-20 09:50:23 -0400

    u

--- visitationbvm/teacher_nis_nfs_setup/setup.sh
+++ visitationbvm/teacher_nis_nfs_setup/setup.sh
@@ -56,6 +56,9 @@ echo install vlc player
 sudo apt-get install vlc
 
 echo for teacher admin of epoptes
+echo for some reason we have to uninstall epoptes first i think it does
+echo not place nice with nis 
+sudo apt-get remove epoptes
 sudo apt-get install epoptes
 echo add student to list epoptes group
 sudo gpasswd -a student epoptes
