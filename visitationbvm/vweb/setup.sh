0aefa48eed6e66deb4a736fec5f759f4d5c67287
jbreslin33 <jbreslin33@localhost>
2012-10-09 10:50:06 -0400

    u

---
+++ visitationbvm/vweb/setup.sh
@@ -0,0 +1,21 @@
+sudo apt-get install apache2
+sudo apt-get install php5
+sudo cp php5.conf /etc/apache2/mods-enabled
+sudo cp 000-default /etc/apache2/sites-enabled
+sudo /etc/init.d/apache2 restart
+sudo chmod 777 /var/www/
+
+sudo apt-get install mercurial
+
+sudo apt-get install postgresql
+sudo apt-get install phppgadmin
+
+sudo cp apache2.conf /etc/apache2
+sudo cp apache.conf /etc/phppgadmin
+
+sudo service apache2 reload 
+sudo cp config.inc.php /etc/phppgadmin
+
+sudo cp postgresql.conf /etc/postgresql/9.1/main
+
+
