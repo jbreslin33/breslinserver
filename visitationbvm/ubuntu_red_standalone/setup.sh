40edf3b6dc1499bac8428d91edb3d9a3ab6793b2
jbreslin33 <jbreslin33@localhost>
2012-10-22 16:01:52 -0400

    u

--- visitationbvm/ubuntu_red_standalone/setup.sh
+++ visitationbvm/ubuntu_red_standalone/setup.sh
@@ -21,7 +21,11 @@ echo change login
 sudo cp ../ubuntu_standalone/lightdm.conf /etc/lightdm
 
 echo power off on lid closed
-sudo cp lidbtn.sh /etc/acpi
+sudo cp lidbtn.sh /etc/acpi/events
+sudo cp pb.sh /etc/acpi
+sudo chmod 777 /etc/acpi/pb.sh
+sudo +x /etc/acpi/pb.sh
+
 
 echo install epoptes-client
 sudo apt-get install epoptes-client
