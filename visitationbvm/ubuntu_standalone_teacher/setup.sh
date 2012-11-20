5b571dacb5e0a6a3e03a2821f0b96cd2ddc5baaa
jbreslin33 <jbreslin33@localhost>
2012-11-20 16:30:49 -0500

    u

--- visitationbvm/ubuntu_standalone_teacher/setup.sh
+++ visitationbvm/ubuntu_standalone_teacher/setup.sh
@@ -6,19 +6,19 @@ echo update system
 sudo apt-get dist-upgrade
 
 echo update hosts file
-cp hosts /etc
+cp ../ubuntu_standalone/hosts /etc
 
 echo cron jobs
-sudo cp crontab /etc
+sudo cp ../ubuntu_standalone/crontab /etc
 sudo mkdir /scripts
-sudo cp mount_nfs.sh /scripts 
+sudo cp ../ubuntu_standalone/mount_nfs.sh /scripts 
 sudo chmod 777 /mnt
 
 echo install nfs-common
 sudo apt-get install nfs-common
 
 echo change login
-sudo cp lightdm.conf /etc/lightdm
+sudo cp ../ubuntu_standalone/lightdm.conf /etc/lightdm
 
 echo install epoptes-client
 sudo apt-get install epoptes-client
@@ -32,6 +32,19 @@ sudo apt-get install tuxpaint
 echo vlc
 sudo apt-get install vlc
 
+echo teacher specific setup
+echo for teacher admin of epoptes
+echo for some reason we have to uninstall epoptes first i think it does
+echo not place nice with nis
+sudo apt-get remove epoptes
+sudo apt-get install epoptes
+echo add student to list epoptes group
+sudo gpasswd -a student epoptes
+sudo gpasswd -a v1201 epoptes
+
+echo install clusterssh
+sudo apt-get install clusterssh
+
 echo add users
 sudo newusers ../add_student_scripts/staff.txt
 sudo newusers ../add_student_scripts/v1300.txt
@@ -43,6 +56,5 @@ sudo newusers ../add_student_scripts/v1800.txt
 sudo newusers ../add_student_scripts/v1900.txt
 sudo newusers ../add_student_scripts/v2000.txt
 sudo newusers ../add_student_scripts/v2100.txt
-
 
 echo complete you can now reboot and login 
