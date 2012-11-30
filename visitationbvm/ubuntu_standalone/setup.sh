6ff3bf919a2788717beeb2cdd09ac552295ae6b8
jbreslin33 <jbreslin33@localhost>
2012-11-30 11:26:50 -0500

    u

--- visitationbvm/ubuntu_standalone/setup.sh
+++ visitationbvm/ubuntu_standalone/setup.sh
@@ -17,6 +17,9 @@ sudo chmod 777 /mnt
 echo install nfs-common
 sudo apt-get install nfs-common
 
+echo install smbfs
+sudo apt-get install smbfs
+
 echo change login
 sudo cp lightdm.conf /etc/lightdm
 
