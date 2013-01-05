7558e0e4efcbc9c38c5e70bf6df7ac53b02cc069
jbreslin33 <jbreslin33@localhost>
2013-01-05 14:18:37 -0500

    ogre setup

---
+++ ogre/setup.sh
@@ -0,0 +1,16 @@
+sudo apt-get install build-essential automake libtool
+sudo apt-get install libfreetype6-dev libfreeimage-dev libzzip-dev libxrandr-dev libxaw7-dev freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev
+sudo apt-get install nvidia-cg-toolkit libois-dev libboost-thread-dev
+sudo apt-get install doxygen graphviz libcppunit-dev
+
+sudo apt-get install mercurial
+sudo apt-get install cmake
+mkdir ../../sandbox
+cd ../../sandbox
+hg clone https://bitbucket.org/sinbad/ogre/ -u v1-7
+cd ogre
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
