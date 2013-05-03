5d9306de1ffa10bbee0c1ce5f8b596377abc1904
jbreslin33 <jbreslin33@localhost>
2013-05-03 13:38:55 -0400

    u

--- visitationbvm/cluster_scripts/r32.sh
+++ visitationbvm/cluster_scripts/r32.sh
@@ -1,1 +1,1 @@
-clusterssh student@r3201 student@3202 student@r3203 student@r3204 student@r3205 student@r3206 student@r3207 student@r3208
+clusterssh student@r3201 student@r3202 student@r3203 student@r3204 student@r3205 student@r3206 student@r3207 student@r3208
