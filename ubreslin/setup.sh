92d6c2262ea6fe3e4d3dc04f624e4aaa19fdde01
jbreslin33 <jbreslin33@localhost>
2013-06-20 23:18:12 -0400

    updated to full screen smaall screen labtop

--- ubreslin/setup.sh
+++ ubreslin/setup.sh
@@ -8,6 +8,6 @@ sudo apt-get install mixxx
 sudo mkdir /amusic
 mkdir /home/jbreslin/.mixxx
 sudo chmod 777 -R .mixxx
-sudo chown jbreslin /home/jbreslin/.mixxx
-sudo chgrp jbreslin /home/jbreslin/.mixxx
+sudo chown -R jbreslin /home/jbreslin/.mixxx
+sudo chgrp -R jbreslin /home/jbreslin/.mixxx
 sudo cp .mixxx/* /home/jbreslin/.mixxx/
