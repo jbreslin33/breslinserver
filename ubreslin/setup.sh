1b969c2bb6d581ff7a8f8e65d853cb57109ef949
jbreslin33 <jbreslin33@localhost>
2013-06-24 19:17:44 -0400

    uBRESLIN

--- ubreslin/setup.sh
+++ ubreslin/setup.sh
@@ -1,15 +1,15 @@
-echo updates
+echo UBRESLIN: UPDATES 
 sudo dpkg --configure -a
 sudo apt-get dist-upgrade
 
-#source control
+echo UBRESLIN: SOURCE CONTROL 
 sudo apt-get install git
 
-#build programs
-sudo apt-get install build-essentials
+echo UBRESLIN: BUILD TOOLS 
+sudo apt-get install build-essential
 sudo apt-get install openjdk-7-jdk
 
-#projects
+echo UBRESLIN: PROJECTS 
 echo projects
 cd /home/$1/sandbox
 
@@ -26,10 +26,10 @@ hg clone https://lbreslin6@code.google.com/p/breslinnetwork/
 hg clone https://lbreslin6@code.google.com/p/lukes-webpage/
 fi
 
-echo admin 
+echo UBRESLIN: ADMIN 
 sudo apt-get install ssh
 
-echo dj
+echo UBRESLIN: DJ 
 sudo apt-get install mixxx
 sudo mkdir /amusic
 mkdir /home/$1/.mixxx
@@ -38,7 +38,7 @@ sudo chown -R $1 /home/$1/.mixxx
 sudo chgrp -R $1 /home/$1/.mixxx
 sudo cp .mixxx/* /home/$1/.mixxx/
 
-#apache2
+echo UBRESLIN: APACHE 
 sudo apt-get install apache2
 cd /home/$1/sandbox/breslinserver/ubreslin
 sudo cp apache2.conf /etc/apache2
@@ -47,10 +47,12 @@ sudo cp 000-default /etc/apache2/sites-enabled
 sudo /etc/init.d/apache2 restart
 sudo chmod 777 /home/$1/sandbox/baseapplication/src
 
-#php5
+echo UBRESLIN: PHP 
+sudo apt-get install apache2
 sudo apt-get install php5
 
-#postgresql
+echo UBRESLIN: POSTGRESQL 
+sudo apt-get install apache2
 sudo apt-get install postgresql
 sudo apt-get install phppgadmin
 sudo apt-get install libpq-dev
@@ -59,7 +61,7 @@ sudo service apache2 reload
 sudo cp config.inc.php /etc/phppgadmin
 sudo cp postgresql.conf /etc/postgresql/9.1/main
 
-#arduino
+echo UBRESLIN: ARDUINO 
 sudo apt-get install gcc-avr avr-libc
 sudo apt-get install make
 sudo apt-get install ant
@@ -69,7 +71,7 @@ git clone git://github.com/arduino/Arduino.git
 cd /home/$1/sandbox/Arduino/build
 ant
 
-echo install hardware
+echo UBRESLIN: install hardware for arduino uno
 dmesg tail
 ls -l /dev/ttyACM0
 usermod -a -G dialout jbreslin
@@ -77,20 +79,20 @@ sudo usermod -a -G dialout jbreslin
 ls -ld /run/lock
 sudo chmod o+rwx /run/lock/
 
-echo ADDITIONAL MANUAL COMMANDS
-echo run these next commands manually
+echo UBRESLIN: ADDITIONAL MANUAL COMMANDS
+echo UBRESLIN: run these next commands manually
 
-echo postgres stuff...
-echo sudo -u postgres psql postgres
-echo \password postgres
+echo UBRESLIN: postgres stuff...
+echo UBRESLIN: sudo -u postgres psql postgres
+echo UBRESLIN: \password postgres
 
-echo for abcyandyou db
-echo sudo -u postgres createdb abcandyou 
-echo sudo su postgres
-echo psql -d abcandyou -f db_build.sql
+echo UBRESLIN: for abcyandyou db
+echo UBRESLIN: sudo -u postgres createdb abcandyou 
+echo UBRESLIN: sudo su postgres
+echo UBRESLIN: psql -d abcandyou -f db_build.sql
 
-echo Arduino stuff...
-echo to just run arduino development environment
-echo cd /home/USER/sandbox/Arduino/build
-echo ant run
+echo UBRESLIN: Arduino stuff...
+echo UBRESLIN: to just run arduino development environment
+echo UBRESLIN: cd /home/USER/sandbox/Arduino/build
+echo UBRESLIN: ant run
 
