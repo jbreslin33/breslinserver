4641d103940573301078750d5c1dd48f9cd3f613
jbreslin33 <jbreslin33@localhost>
2013-08-23 16:05:56 -0400

    finishing moodle install

--- ubreslin2/setup.sh
+++ ubreslin2/setup.sh
@@ -192,3 +192,15 @@ echo then a pound prompt run CREATE USER moodleuser WITH PASSWORD Iggles_13 in t
 echo CREATE DATABASE moodle WITH OWNER moodleuser;
 echo mkdir /home/$1/sandbox/moodledata
 echo sudo chmod 0777 /home/$1/sandbox/moodledata 
+echo sudo chown www-data /home/$1/sandbox/moodle
+echo cd /home/$1/sandbox/moodle/admin/cli/
+echo sudo -u www-data /usr/bin/php install.php 
+echo defaults and for web address use.. http://visflip.com
+echo for db user use moodleuser
+echo db pass Iggles_13
+echo sudo chown -R root /home/$1/sandbox/moodle
+echo goto visflip.com
+echo sudo apt-get install php5-curl
+echo sudo /etc/init.d/apache2 restart
+echo sudo apt-get install php5-gd
+echo sudo /etc/init.d/apache2 restart
