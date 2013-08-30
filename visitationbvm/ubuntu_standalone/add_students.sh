937f00e786db5fe78c8fcb60db17feae18450e12
jbreslin33 <jbreslin33@localhost>
2013-08-30 11:02:04 -0400

    add students

--- visitationbvm/ubuntu_standalone/add_students.sh
+++ visitationbvm/ubuntu_standalone/add_students.sh
@@ -1,10 +1,9 @@
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/staff.txt
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/v1400.txt
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/v1500.txt
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/v1600.txt
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/v1700.txt
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/v1800.txt
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/v1900.txt
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/v2000.txt
-sudo newusers /home/student/breslinserver/visitationbvm/add_student_scripts/v2100.txt
-
+sudo newusers v1400.txt
+sudo newusers v1500.txt
+sudo newusers v1600.txt
+sudo newusers v1700.txt
+sudo newusers v1800.txt
+sudo newusers v1900.txt
+sudo newusers v2000.txt
+sudo newusers v2100.txt
+sudo newusers staff.txt
