8433bb54e20f11b526f3f1790e282a495de123b0
jbreslin33 <jbreslin33@localhost>
2013-08-31 14:49:18 -0400

    u

--- ubreslin2/setup.sh
+++ ubreslin2/setup.sh
@@ -156,6 +156,8 @@ echo
 echo ---------------------------------------------
 echo UBRESLIN: MIXXX 
 echo ---------------------------------------------
+sudo mkdir /amusic
+sudo chmod -R 777 /amusic
 mkdir /home/$1/.mixxx
 mkdir /home/$1/.mixxx/midi
 cp /home/$1/sandbox/breslinserver/ubreslin2/.mixxx/* /home/$1/.mixxx/
