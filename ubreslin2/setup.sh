76d558ea594aa881dff70998f6c4d0a63cb5d573
jbreslin33 <jbreslin33@localhost>
2013-09-01 13:00:53 -0400

    took out node stuff

--- ubreslin2/setup.sh
+++ ubreslin2/setup.sh
@@ -114,8 +114,8 @@ cd node
 ./configure
 sudo make
 sudo make install
-curl http://npmjs.org/install.sh
-sudo npm install -d
+echo XXcurl http://npmjs.org/install.sh
+echo XXsudo npm install -d
 sudo chmod -R 777 /home/$1/sandbox/baseapplication/src/web/insert/upload
 fi
 echo create db abcandyou
