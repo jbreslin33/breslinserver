0dca4569a869052c445a1923e4f998da7eb287ea
jbreslin33 <jbreslin33@localhost>
2013-09-01 15:12:02 -0400

    moved out moodele to own file

--- ubreslin2/setup.sh
+++ ubreslin2/setup.sh
@@ -125,15 +125,6 @@ sudo /etc/init.d/apache2 restart
 
 echo
 echo ---------------------------------------------
-echo UBRESLIN: MOODLE 
-echo ---------------------------------------------
-cd /home/$1/sandbox
-git clone -b MOODLE_24_STABLE git://git.moodle.org/moodle.git
-sudo chown -R root /home/$1/sandbox/moodle/
-sudo chmod -R 0755 /home/$1/sandbox/moodle/
-
-echo
-echo ---------------------------------------------
 echo UBRESLIN: MIXXX 
 echo ---------------------------------------------
 sudo mkdir /amusic
@@ -176,29 +167,3 @@ echo signup
 echo viso mibesfat
 echo insert upload visitation addstudentscrips v1300.txt
 
-echo
-echo --------------------------------------------
-echo UBRESLIN: MOODLE 
-echo --------------------------------------------
-echo sudo -u postgres psql postgres
-echo then a pound prompt run CREATE USER moodleuser WITH PASSWORD Iggles_13 in tics;
-echo CREATE DATABASE moodle WITH OWNER moodleuser;
-echo mkdir /home/$1/sandbox/moodledata
-echo sudo chmod 0777 /home/$1/sandbox/moodledata 
-echo sudo chown www-data /home/$1/sandbox/moodle
-echo cd /home/$1/sandbox/moodle/admin/cli/
-echo sudo -u www-data /usr/bin/php install.php 
-echo defaults and for web address use.. http://visflip.com
-echo for db user use moodleuser
-echo db pass Iggles_13
-echo sudo chown -R root /home/$1/sandbox/moodle
-echo goto visflip.com
-echo you will probably get errors if so the following are the fixes
-echo sudo apt-get install php5-curl
-echo sudo /etc/init.d/apache2 restart
-echo sudo apt-get install php5-gd
-echo sudo /etc/init.d/apache2 restart
-echo  sudo apt-get install php5-xmlrpc
-echo sudo /etc/init.d/apache2 restart
-echo sudo apt-get install php5-intl
-echo sudo /etc/init.d/apache2 restart
