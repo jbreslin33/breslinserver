64c1e47b53daaf2a248f8398d1e2c64cb1a48522
jbreslin33 <jbreslin33@localhost>
2013-09-02 17:06:15 -0400

    mixx

--- ubreslin2/setup_mixxx.sh
+++ ubreslin2/setup_mixxx.sh
@@ -9,5 +9,6 @@ mkdir /home/$1/.mixxx
 mkdir /home/$1/.mixxx/midi
 cp /home/$1/sandbox/breslinserver/ubreslin2/.mixxx/* /home/$1/.mixxx/
 cp /home/$1/sandbox/breslinserver/ubreslin2/.mixxx/midi/* /home/$1/.mixxx/midi/
-
+echo if external then plug in drive and run ./setup_symbolic_external.sh
+if using local drive then copy music from external to /amusic
 
