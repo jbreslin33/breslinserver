93ad87801b901659cc29cd185392524b8f9f6526
jbreslin33 <jbreslin33@localhost>
2013-11-06 15:07:41 -0500

    u

--- visitationbvm/ubuntu_standalone/setup.sh
+++ visitationbvm/ubuntu_standalone/setup.sh
@@ -10,20 +10,8 @@ sudo cp hosts /etc
 
 echo cron jobs
 sudo cp crontab /etc
-sudo mkdir /scripts
-sudo cp mount_nfs.sh /scripts 
-sudo cp add_students.sh /scripts 
-echo copy student add users scripts 
-sudo cp ../add_student_scripts/v1400.txt /scripts 
-sudo cp ../add_student_scripts/v1500.txt /scripts 
-sudo cp ../add_student_scripts/v1600.txt /scripts 
-sudo cp ../add_student_scripts/v1700.txt /scripts 
-sudo cp ../add_student_scripts/v1800.txt /scripts 
-sudo cp ../add_student_scripts/v1900.txt /scripts 
-sudo cp ../add_student_scripts/v2000.txt /scripts 
-sudo cp ../add_student_scripts/v2100.txt /scripts 
-sudo cp ../add_student_scripts/staff.txt /scripts 
 
+echo mounting
 sudo chmod 777 /mnt
 
 echo install nfs-common
@@ -53,24 +41,12 @@ sudo apt-get remove empathy
 echo install pinta paint program
 sudo apt-get install pinta
 
-echo install risk
-sudo apt-get install ksirk
-
 echo install java
 sudo apt-get install openjdk-7-jre
 sudo apt-get install icedtea-7-plugin
 
 echo add users
-sudo newusers ../add_student_scripts/staff.txt
-sudo newusers ../add_student_scripts/v1300.txt
-sudo newusers ../add_student_scripts/v1400.txt
-sudo newusers ../add_student_scripts/v1500.txt
-sudo newusers ../add_student_scripts/v1600.txt
-sudo newusers ../add_student_scripts/v1700.txt
-sudo newusers ../add_student_scripts/v1800.txt
-sudo newusers ../add_student_scripts/v1900.txt
-sudo newusers ../add_student_scripts/v2000.txt
-sudo newusers ../add_student_scripts/v2100.txt
+sudo newusers ../add_student_scripts/add_viso.txt
 
 echo complete you can now reboot and login 
 echo IMPORTANT!!  if this is lubuntu then cd into lubuntu_standalone and run setup.sh
