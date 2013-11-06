883f912719bceefdfe7cce6d8143e55789c50cf6
jbreslin33 <jbreslin33@localhost>
2013-11-06 15:04:35 -0500

    u

--- visitationbvm/ubuntu_standalone/add_students.sh
+++ visitationbvm/ubuntu_standalone/add_students.sh
@@ -1,9 +1,1 @@
-sudo newusers v1400.txt
-sudo newusers v1500.txt
-sudo newusers v1600.txt
-sudo newusers v1700.txt
-sudo newusers v1800.txt
-sudo newusers v1900.txt
-sudo newusers v2000.txt
-sudo newusers v2100.txt
-sudo newusers staff.txt
+sudo newusers ../add_student_scripts/add_viso.txt
