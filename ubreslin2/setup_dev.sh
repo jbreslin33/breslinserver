9b1374034b142ad68b25ef6bcb723af1a4b91db6
jbreslin33 <jbreslin33@localhost>
2014-01-19 17:14:26 -0500

    cp new default

---
+++ ubreslin2/setup_dev.sh
@@ -0,0 +1,103 @@
+echo
+echo --------------------------------------------
+echo UBRESLIN: UPDATES 
+echo --------------------------------------------
+sudo apt-get dist-upgrade
+
+echo
+echo --------------------------------------------
+echo UBRESLIN: INSTALL PROGRAMS 
+echo --------------------------------------------
+sudo apt-get install build-essential
+sudo apt-get install openjdk-7-jdk
+sudo apt-get install curl
+sudo apt-get install gcc-avr avr-libc
+sudo apt-get install make
+sudo apt-get install ant
+sudo apt-get install cmake
+sudo apt-get install automake
+sudo apt-get install libtool
+sudo apt-get install libfreetype6-dev libfreeimage-dev libzzip-dev libxrandr-dev libxaw7-dev freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev
+sudo apt-get install nvidia-cg-toolkit libois-dev libboost-thread-dev
+sudo apt-get install doxygen graphviz libcppunit-dev
+sudo apt-get install git
+sudo apt-get install ssh 
+sudo apt-get install apache2 
+sudo apt-get install php5 
+sudo apt-get install postgresql
+sudo apt-get install phppgadmin
+sudo apt-get install libpq-dev
+sudo apt-get install vlc
+
+echo
+echo ---------------------------------------------
+echo UBRESLIN: PROJECTS
+echo ---------------------------------------------
+echo projects
+cd /home/$1/sandbox
+
+if [ "$1" = "jbreslin" ]; then
+hg clone https://jbreslin33@code.google.com/p/breslininput/
+sudo chown -R $1:$2 /home/$1/sandbox/breslininput/
+hg clone https://jbreslin33@code.google.com/p/breslinnetwork/
+sudo chown -R $1:$2 /home/$1/sandbox/breslinnetwork/
+hg clone https://jbreslin33@code.google.com/p/breslintalker/
+sudo chown -R $1:$2 /home/$1/sandbox/breslintalker/
+hg clone https://jbreslin33@code.google.com/p/breslinclient/
+sudo chown -R $1:$2 /home/$1/sandbox/breslinclient/
+hg clone https://jbreslin33@code.google.com/p/breslinmathracer/
+sudo chown -R $1:$2 /home/$1/sandbox/breslinmathracer/
+hg clone https://jbreslin33@code.google.com/p/armygame/
+sudo chown -R $1:$2 /home/$1/sandbox/armygame/
+fi
+
+if [ "$1" = "lbreslin" ]; then
+hg clone https://lbreslin6@code.google.com/p/breslininput/
+sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslininput/
+hg clone https://lbreslin6@code.google.com/p/breslinnetwork/
+sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslinnetwork/
+hg clone https://lbreslin6@code.google.com/p/breslintalker/
+sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslintalker/
+hg clone https://lbreslin6@code.google.com/p/breslinclient/
+sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslinclient/
+hg clone https://lbreslin6@code.google.com/p/breslinmathracer/
+sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslinmathracer/
+hg clone https://lbreslin6@code.google.com/p/armygame/
+sudo chown -R lbreslin:lbreslin /home/$1/sandbox/armygame/
+hg clone https://lbreslin6@code.google.com/p/lukes-webpage/
+sudo chown -R lbreslin:lbreslin /home/$1/sandbox/lukes-webpage/
+fi
+
+
+echo create db jamesanthony
+sudo -u postgres createdb jamesanthonybreslin
+
+echo
+echo ---------------------------------------------
+echo UBRESLIN: COPY FILES 
+echo ---------------------------------------------
+echo sudo cp /home/$1/sandbox/breslinserver/ubreslin2/apache2.conf /etc/apache2
+echo skip next one
+echo sudo cp /home/$1/sandbox/breslinserver/ubreslin2/php5.conf /etc/apache2/mods-enabled
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default /etc/apache2/sites-available
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/apache.conf /etc/phppgadmin
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/config.inc.php /etc/phppgadmin
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/postgresql.conf /etc/postgresql/9.1/main
+sudo service apache2 reload
+sudo /etc/init.d/apache2 restart
+
+echo
+echo --------------------------------------------
+echo UBRESLIN: ADDITIONAL MANUAL COMMANDS
+echo --------------------------------------------
+echo UBRESLIN: run these next commands manually
+
+echo
+echo --------------------------------------------
+echo UBRESLIN: POSTGRESQL
+echo --------------------------------------------
+echo sudo -u postgres psql postgres
+echo  backslashpassword postgres dont forget to put a backslash before password, make postgres pass mibesfat
+
+echo for adding to db 
+echo sudo chmod -R 777 web/insert/
