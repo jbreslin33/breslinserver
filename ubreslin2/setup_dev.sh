a5b7d6a8addfd832d4f21f78149b57a23bfbde1a
jbreslin33 <jbreslin33@localhost>
2014-04-27 17:47:42 -0400

    dev

--- ubreslin2/setup_dev.sh
+++ ubreslin2/setup_dev.sh
@@ -49,6 +49,7 @@ hg clone https://jbreslin33@code.google.com/p/breslinmathracer/
 sudo chown -R $1:$2 /home/$1/sandbox/breslinmathracer/
 hg clone https://jbreslin33@code.google.com/p/armygame/
 sudo chown -R $1:$2 /home/$1/sandbox/armygame/
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_jbreslin /etc/apache2/sites-available/default
 fi
 
 if [ "$1" = "lbreslin" ]; then
@@ -66,8 +67,26 @@ hg clone https://lbreslin6@code.google.com/p/armygame/
 sudo chown -R lbreslin:lbreslin /home/$1/sandbox/armygame/
 hg clone https://lbreslin6@code.google.com/p/lukes-webpage/
 sudo chown -R lbreslin:lbreslin /home/$1/sandbox/lukes-webpage/
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_lbreslin /etc/apache2/sites-available/default
 fi
 
+if [ "$1" = "bbreslin" ]; then
+hg clone https://bbreslin99@code.google.com/p/breslininput/
+sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslininput/
+hg clone https://bbreslin99@code.google.com/p/breslinnetwork/
+sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslinnetwork/
+hg clone https://bbreslin99@code.google.com/p/breslintalker/
+sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslintalker/
+hg clone https://bbreslin99@code.google.com/p/breslinclient/
+sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslinclient/
+hg clone https://bbreslin99@code.google.com/p/breslinmathracer/
+sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslinmathracer/
+hg clone https://bbreslin99@code.google.com/p/armygame/
+sudo chown -R bbreslin:bbreslin /home/$1/sandbox/armygame/
+hg clone https://bbreslin99@code.google.com/p/lukes-webpage/
+sudo chown -R bbreslin:bbreslin /home/$1/sandbox/lukes-webpage/
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_bbreslin /etc/apache2/sites-available/default
+fi
 
 echo create db jamesanthony
 sudo -u postgres createdb jamesanthonybreslin
@@ -76,10 +95,6 @@ echo
 echo ---------------------------------------------
 echo UBRESLIN: COPY FILES 
 echo ---------------------------------------------
-echo sudo cp /home/$1/sandbox/breslinserver/ubreslin2/apache2.conf /etc/apache2
-echo skip next one
-echo sudo cp /home/$1/sandbox/breslinserver/ubreslin2/php5.conf /etc/apache2/mods-enabled
-sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default /etc/apache2/sites-available
 sudo cp /home/$1/sandbox/breslinserver/ubreslin2/apache.conf /etc/phppgadmin
 sudo cp /home/$1/sandbox/breslinserver/ubreslin2/config.inc.php /etc/phppgadmin
 sudo cp /home/$1/sandbox/breslinserver/ubreslin2/postgresql.conf /etc/postgresql/9.1/main
