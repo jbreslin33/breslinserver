184324221a7de797ffcab0698ca60e58814a842a
jbreslin33 <jbreslin33@localhost>
2014-05-02 16:37:09 -0400

    added a setup fo rhome

---
+++ visitationbvm/ubuntu_standalone/setup_home.sh
@@ -0,0 +1,36 @@
+sudo dpkg --configure -a
+sudo apt-get -y update 
+sudo apt-get -y install 
+sudo apt-get -y upgrade
+
+echo sudo cp hosts /etc
+echo sudo cp crontab /etc
+echo sudo chmod 777 /mnt
+sudo cp lightdm.conf /etc/lightdm
+
+sudo apt-get -y install ssh
+sudo apt-get -y install nfs-common
+sudo apt-get -y install cifs-utils
+sudo apt-get -y install tuxmath
+sudo apt-get -y install tuxtype
+sudo apt-get -y install tuxpaint
+sudo apt-get -y install vlc
+sudo apt-get -y install pinta
+sudo apt-get -y install openjdk-7-jre
+sudo apt-get -y install icedtea-7-plugin
+sudo apt-get -y install libreoffice
+sudo apt-get -y install arduino arduino-core
+sudo apt-get -y install chromium-browser
+sudo apt-get -y remove empathy
+sudo apt-get -y install epoptes-client
+sudo apt-get -y install lubuntu-desktop 
+sudo apt-get -y install kubuntu-desktop 
+sudo apt-get -y install xubuntu-desktop 
+sudo apt-get -y install awesome 
+
+echo sudo epoptes-client -c
+sudo newusers ../add_student_scripts/add_viso.txt
+
+echo complete you can now reboot and login 
+echo IMPORTANT!!  if this is lubuntu then cd into lubuntu_standalone and run setup.sh
+echo IMPORTANT AS WELL if this is a red laptop then you have to next cd to ubuntu_red_standalone and run setup
