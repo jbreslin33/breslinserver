f660b061d533e88c38c31aab9d422dc6fedb8378
jbreslin33 <jbreslin33@localhost>
2014-05-17 12:43:03 -0400

    modified virtual hosts

--- ubreslin2/setup_dev.sh
+++ ubreslin2/setup_dev.sh
@@ -2,32 +2,32 @@ echo
 echo --------------------------------------------
 echo UBRESLIN: UPDATES 
 echo --------------------------------------------
-sudo apt-get dist-upgrade
+sudo apt-get -y dist-upgrade
 
 echo
 echo --------------------------------------------
 echo UBRESLIN: INSTALL PROGRAMS 
 echo --------------------------------------------
-sudo apt-get install build-essential
-sudo apt-get install openjdk-7-jdk
-sudo apt-get install curl
-sudo apt-get install gcc-avr avr-libc
-sudo apt-get install make
-sudo apt-get install ant
-sudo apt-get install cmake
-sudo apt-get install automake
-sudo apt-get install libtool
-sudo apt-get install libfreetype6-dev libfreeimage-dev libzzip-dev libxrandr-dev libxaw7-dev freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev
-sudo apt-get install nvidia-cg-toolkit libois-dev libboost-thread-dev
-sudo apt-get install doxygen graphviz libcppunit-dev
-sudo apt-get install git
-sudo apt-get install ssh 
-sudo apt-get install apache2 
-sudo apt-get install php5 
-sudo apt-get install postgresql
-sudo apt-get install phppgadmin
-sudo apt-get install libpq-dev
-sudo apt-get install vlc
+sudo apt-get -y install build-essential
+sudo apt-get -y install openjdk-7-jdk
+sudo apt-get -y install curl
+sudo apt-get -y install gcc-avr avr-libc
+sudo apt-get -y install make
+sudo apt-get -y install ant
+sudo apt-get -y install cmake
+sudo apt-get -y install automake
+sudo apt-get -y install libtool
+sudo apt-get -y install libfreetype6-dev libfreeimage-dev libzzip-dev libxrandr-dev libxaw7-dev freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev
+sudo apt-get -y install nvidia-cg-toolkit libois-dev libboost-thread-dev
+sudo apt-get -y install doxygen graphviz libcppunit-dev
+sudo apt-get -y install git
+sudo apt-get -y install ssh 
+sudo apt-get -y install apache2 
+sudo apt-get -y install php5 
+sudo apt-get -y install postgresql
+sudo apt-get -y install phppgadmin
+sudo apt-get -y install libpq-dev
+sudo apt-get -y install vlc
 
 echo
 echo ---------------------------------------------
@@ -49,7 +49,7 @@ hg clone https://jbreslin33@code.google.com/p/breslinmathracer/
 sudo chown -R $1:$2 /home/$1/sandbox/breslinmathracer/
 hg clone https://jbreslin33@code.google.com/p/armygame/
 sudo chown -R $1:$2 /home/$1/sandbox/armygame/
-sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_jbreslin /etc/apache2/sites-available/default
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_jbreslin /etc/apache2/sites-available/000-default.conf
 fi
 
 if [ "$1" = "lbreslin" ]; then
@@ -67,7 +67,7 @@ hg clone https://lbreslin6@code.google.com/p/armygame/
 sudo chown -R lbreslin:lbreslin /home/$1/sandbox/armygame/
 hg clone https://lbreslin6@code.google.com/p/lukes-webpage/
 sudo chown -R lbreslin:lbreslin /home/$1/sandbox/lukes-webpage/
-sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_lbreslin /etc/apache2/sites-available/default
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_lbreslin /etc/apache2/sites-available/000-default.conf
 fi
 
 if [ "$1" = "bbreslin" ]; then
@@ -85,7 +85,7 @@ hg clone https://bbreslin99@code.google.com/p/armygame/
 sudo chown -R bbreslin:bbreslin /home/$1/sandbox/armygame/
 hg clone https://bbreslin99@code.google.com/p/lukes-webpage/
 sudo chown -R bbreslin:bbreslin /home/$1/sandbox/lukes-webpage/
-sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_bbreslin /etc/apache2/sites-available/default
+sudo cp /home/$1/sandbox/breslinserver/ubreslin2/default_bbreslin /etc/apache2/sites-available/000-default.conf
 fi
 
 echo create db jamesanthony
