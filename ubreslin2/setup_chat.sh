55a567482ec38b289100457a3741861e0f4f9249
jbreslin33 <jbreslin33@localhost>
2014-06-09 23:32:58 -0400

    setup chat.

---
+++ ubreslin2/setup_chat.sh
@@ -0,0 +1,8 @@
+#change to sandbox
+git clone https://github.com/guille/chat-example.git
+sudo apt-get install nodejs
+sudo apt-get install npn 
+npm install --save express
+npm install socket.io
+nodejs index.js 
+
