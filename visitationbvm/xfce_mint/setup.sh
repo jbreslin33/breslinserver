a7fb6235d6d6ba78a2ce6d7c66c3015e4f119101
jbreslin33 <jbreslin33@localhost>
2014-06-10 15:26:47 -0400

    complie minetest

--- visitationbvm/xfce_mint/setup.sh
+++ visitationbvm/xfce_mint/setup.sh
@@ -19,11 +19,27 @@ sudo apt-get -y install vlc
 sudo apt-get -y install pinta
 sudo apt-get -y install chromium-browser
 sudo apt-get -y install epoptes-client
-sudo apt-get -y install minetest
+sudo apt-get -y remove minetest
 sudo apt-get -y install remmina
 sudo apt-get -y install rdesktop
 
 sudo epoptes-client -c
 sudo newusers ../add_student_scripts/add_viso.txt
+
+#compile minetest
+sudo mkdir /minetest
+sudo chmod -R 777 /minetest
+cd /minetest
+sudo apt-get install build-essential libirrlicht-dev cmake libbz2-dev libpng12-dev libjpeg8-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev
+wget https://github.com/minetest/minetest/tarball/master -O master.tar.gz
+tar xf master.tar.gz
+cd minetest-minetest-9675d9e/
+cp src/content_cao.cpp /minetest/minetest-minetest-9675d9e/src/
+cd games/
+wget https://github.com/minetest/minetest_game/tarball/master -O master.tar.gz
+tar xf master.tar.gz
+cd ..
+cmake . -DRUN_IN_PLACE=1
+make -j2
 
 echo complete you can now reboot and login 
