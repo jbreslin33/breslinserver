36f3da984c36c596d5b79615d71f1f71327fa829
jbreslin33 <jbreslin33@localhost>
2014-06-26 15:02:47 -0400

    added wcpe.

--- ubreslin2/setup_chat.sh
+++ ubreslin2/setup_chat.sh
@@ -1,8 +1,8 @@
 #change to sandbox
+cd /home/$1/sandbox
 git clone https://github.com/guille/chat-example.git
-sudo apt-get install nodejs
-sudo apt-get install npn 
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
 npm install --save express
 npm install socket.io
 nodejs index.js 
-
