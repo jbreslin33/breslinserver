9cee8f6f6cda8e40dc8137975b968ebc979f0ed0
jbreslin33 <jbreslin33@localhost>
2014-08-07 19:48:21 -0400

    hg

--- ubreslin2/mercurial_server/setup.sh
+++ ubreslin2/mercurial_server/setup.sh
@@ -5,8 +5,9 @@ sudo chown -R www-data:www-data /mercurial/
 sudo cp hgweb.config /mercurial/
 sudo cp hgweb.cgi /mercurial/
 sudo mkdir /etc/apache2/mercurial
-sudo cp mercurial.conf /etc/apache2/
-
+sudo cp mercurial.conf /etc/apache2/mercurial
+echo install some tools
+sudo apt-get install apache2-utils
 echo to setup repo enter manually
 echo cd /mercurial/repositories/
 echo sudo mkdir first_repo
@@ -14,7 +15,9 @@ echo cd first_repo/
 echo sudo hg init 
 echo sudo chown -R www-data:www-data /mercurial
 
+echo for passwords
 cd /mercurial/
+
 
 
 
