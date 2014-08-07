a62a5dffa5c39d1f25b85ac4dfb714d5103cf653
jbreslin33 <jbreslin33@localhost>
2014-08-07 19:25:42 -0400

    added to lukejames

--- ubreslin2/mercurial_server/setup.sh
+++ ubreslin2/mercurial_server/setup.sh
@@ -6,3 +6,15 @@ sudo cp hgweb.config /mercurial/
 sudo cp hgweb.cgi /mercurial/
 sudo mkdir /etc/apache2/mercurial
 sudo cp mercurial.conf /etc/apache2/
+
+echo to setup repo enter manually
+echo cd /mercurial/repositories/
+echo sudo mkdir first_repo
+echo cd first_repo/
+echo sudo hg init 
+echo sudo chown -R www-data:www-data /mercurial
+
+cd /mercurial/
+
+
+
