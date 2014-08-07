0470472021fcd9411f846dd4b9072df6ca0b5258
jbreslin33 <jbreslin33@localhost>
2014-08-07 19:09:06 -0400

    hg

--- ubreslin2/mercurial_server/setup.sh
+++ ubreslin2/mercurial_server/setup.sh
@@ -3,3 +3,6 @@ sudo mkdir /mercurial
 sudo mkdir /mercurial/repositories
 sudo chown -R www-data:www-data /mercurial/
 sudo cp hgweb.config /mercurial/
+sudo cp hgweb.cgi /mercurial/
+sudo mkdir /etc/apache2/mercurial
+sudo cp mercurial.conf /etc/apache2/
