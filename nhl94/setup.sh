84b75a89190a1af58e82fd68462d648355b4a716
jbreslin33 <jbreslin33@localhost>
2014-08-29 16:03:11 -0400

    u

---
+++ nhl94/setup.sh
@@ -0,0 +1,6 @@
+echo
+echo ---------------------------------------------
+echo UBRESLIN: SEGA 
+echo ---------------------------------------------
+sudo dpkg --configure -a
+sudo dpkg -i Gens_2.16.7_i386.deb 
