380069b6569e14a01693b9036bb9b1e568dd2d8e
jbreslin33 <jbreslin33@localhost>
2014-10-23 21:29:19 -0400

    changed mixxx setup

--- ubreslin2/mixxx/setup.sh
+++ ubreslin2/mixxx/setup.sh
@@ -2,20 +2,35 @@ echo
 echo ---------------------------------------------
 echo UBRESLIN: MIXXX
 echo ---------------------------------------------
-sudo dpkg --configure -a
+
+sudo apt-get update
+sudo apt-get install 
+
+sudo apt-get build-dep mixxx 
+sudo apt-get install git scons libqt4-dev libqt4-sql-sqlite libportmidi-dev libopus-dev libshout-dev libtag1-dev libprotobuf-dev protobuf-compiler libvamp-hostsdk3 vamp-plugin-sdk libusb-1.0-0-dev libfftw3-dev libmad0-dev portaudio19-dev libchromaprint-dev librubberband-dev libsqlite3-dev
+
+sudo apt-get install libfaad-dev libmp4v2-dev # required for M4A support
+
+git clone -b 1.11 https://github.com/mixxxdj/mixxx.git
+cd /home/jbreslin/sandbox/mixxx/mixxx
+scons
+
+
+
+echo sudo dpkg --configure -a
+echo sudo apt-get update
+echo sudo apt-get install 
 
 echo to get latest without compiling
-sudo add-apt-repository ppa:mixxx/mixxx
-sudo apt-get update
-sudo apt-get install mixxx libportaudio2
+echo sudo add-apt-repository ppa:mixxx/mixxx
+echo sudo apt-get install mixxx libportaudio2
 
 echo for music
-sudo mkdir /amusic
-sudo chmod -R 777 /amusic
+echo sudo mkdir /amusic
+echo sudo chmod -R 777 /amusic
 
-echo for the db 
-mkdir /home/$1/.mixxx
-mkdir /home/$1/.mixxx/midi
-cp /home/$1/sandbox/breslinserver/ubreslin2/.mixxx/* /home/$1/.mixxx/
-cp /home/$1/sandbox/breslinserver/ubreslin2/.mixxx/midi/* /home/$1/.mixxx/midi/
-echo if external then plug in drive and run setup_symbolic_external
+cd /home/jbreslin/sandbox
+
+
+
+
