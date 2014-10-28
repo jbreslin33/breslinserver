3c8d7d5842a6138e6afda11756ea05565440ce21
jbreslin33 <jbreslin33@localhost>
2014-10-28 11:09:40 -0400

    new password for administroatr

--- visitationbvm/cluster_scripts/windows_server1.sh
+++ visitationbvm/cluster_scripts/windows_server1.sh
@@ -1,1 +1,1 @@
-rdesktop -u administrator -p 'Visitation&Uc0m' -k en-us -a 16 -z -g 1024x768 -5 -P 192.168.2.1
+rdesktop -u administrator -p 'Uc0m&V2014' -k en-us -a 16 -z -g 1024x768 -5 -P 192.168.2.1
