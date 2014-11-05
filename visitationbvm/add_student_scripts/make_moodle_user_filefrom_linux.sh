98f2987c3b9f519124bafea071bc9994e37e174e
jbreslin33 <jbreslin33@localhost>
2014-11-04 19:15:04 -0500

    u

--- visitationbvm/add_student_scripts/make_moodle_user_filefrom_linux.sh
+++ visitationbvm/add_student_scripts/make_moodle_user_filefrom_linux.sh
@@ -1,3 +1,9 @@
-cp add_viso.txt add_school_to_moodle.txt
+echo copy student linux file to a temp file
+cp add_viso.txt add_moodle.txt
+
+echo replace blank space between first and last name with : delimeter
 sed -i 's/ /:/g' add_school_to_moodle.txt
 
+echo get rid of first two lines k and jbreslin
+sed 1d add_moodle.txt > add_school_to_moodle.txt
+sed 1d add_school_to_moodle.txt > add_moodle.txt
