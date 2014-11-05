40c2a961af949a5e50ed272d8c20dcaf2ff79ecc
jbreslin33 <jbreslin33@localhost>
2014-11-04 19:52:53 -0500

    took out middle names

--- visitationbvm/add_student_scripts/make_moodle_user_filefrom_linux.sh
+++ visitationbvm/add_student_scripts/make_moodle_user_filefrom_linux.sh
@@ -1,9 +1,11 @@
 echo copy student linux file to a temp file
-cp add_viso.txt add_moodle.txt
+cp add_viso.txt add_school_to_moodle.txt
 
 echo replace blank space between first and last name with : delimeter
 sed -i 's/ /:/g' add_school_to_moodle.txt
 
 echo get rid of first two lines k and jbreslin
-sed 1d add_moodle.txt > add_school_to_moodle.txt
 sed 1d add_school_to_moodle.txt > add_moodle.txt
+sed 1d add_moodle.txt > add_school_to_moodle.txt
+cp add_school_to_moodle.txt add_moodle.txt
+sed -i '1s/^/username:password:email:a:firstname:lastname:b:c\n/' add_moodle.txt 
