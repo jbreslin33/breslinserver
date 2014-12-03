b0c1cade8753efb91377e165f7cc7651168e2e9b
jbreslin33 <jbreslin33@localhost>
2014-12-02 22:22:32 -0500

    u

---
+++ arduinocode/serial.sh
@@ -0,0 +1,2 @@
+stty -F /dev/ttyACM2 cs8 115200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+echo "Hello Arduino" > /dev/ttyACM2
