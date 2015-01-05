8c530d39eadef2dd995479af2bb797e2dc66df47
jbreslin33 <jbreslin33@localhost>
2015-01-04 22:39:09 -0500

    u

--- ubreslin2/moodle/setup.sh
+++ ubreslin2/moodle/setup.sh
@@ -1,9 +1,15 @@
-echo cd /home/jbreslin/sandbox
-echo git clone --depth=1 -b MOODLE_28_STABLE --single-branch git://git.moodle.org/moodle.git 
-echo sudo chown -R root /home/jbreslin/sandbox/moodle
-echo sudo chmod -R 0755 /home/jbreslin/sandbox/moodle
-echo sudo find /home/jbreslin/sandbox/moodle -type f -exec chmod 0644 {} \;
+sudo apt-get install php5-curl
+sudo /etc/init.d/apache2 restart 
+cd /home/jbreslin/sandbox
+git clone --depth=1 -b MOODLE_28_STABLE --single-branch git://git.moodle.org/moodle.git 
+sudo chown -R root /home/jbreslin/sandbox/moodle
+sudo chmod -R 0755 /home/jbreslin/sandbox/moodle
+sudo find /home/jbreslin/sandbox/moodle -type f -exec chmod 0644 {} \;
 echo chmod -R "www-data allow read,delete,write,append,file_inherit,directory_inherit" /home/jbreslin/sandbox/moodle
-echo sudo -u postgres psql -f setup.sql
+sudo -u postgres psql -f setup.sql
 mkdir /home/jbreslin/sandbox/moodledata
 chmod 0777 /home/jbreslin/sandbox/moodledata
+sudo chown www-data /home/jbreslin/sandbox/moodle
+cd /home/jbreslin/sandbox/moodle
+cd /home/jbreslin/sandbox/moodle/admin/cli/
+sudo -u www-data /usr/bin/php install.php
