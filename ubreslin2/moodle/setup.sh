15a63b4b85e286edc864f49f881d12a4d365835c
jbreslin33 <jbreslin33@localhost>
2015-01-04 22:21:25 -0500

    u

--- ubreslin2/moodle/setup.sh
+++ ubreslin2/moodle/setup.sh
@@ -1,5 +1,7 @@
-cd /home/jbreslin/sandbox
+echo cd /home/jbreslin/sandbox
 echo git clone --depth=1 -b MOODLE_28_STABLE --single-branch git://git.moodle.org/moodle.git 
-sudo chown -R root /home/jbreslin/sandbox/moodle
-sudo chmod -R 0755 /home/jbreslin/sandbox/moodle
-sudo find /home/jbreslin/sandbox/moodle -type f -exec chmod 0644 {} \;
+echo sudo chown -R root /home/jbreslin/sandbox/moodle
+echo sudo chmod -R 0755 /home/jbreslin/sandbox/moodle
+echo sudo find /home/jbreslin/sandbox/moodle -type f -exec chmod 0644 {} \;
+echo chmod -R "www-data allow read,delete,write,append,file_inherit,directory_inherit" /home/jbreslin/sandbox/moodle
+sudo -u postgres psql -f setup.sql
