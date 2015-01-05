4d32e89649f5ccff80aeebf1256c5784b1c330f8
jbreslin33 <jbreslin33@localhost>
2015-01-04 22:29:39 -0500

    moodle data

--- ubreslin2/moodle/setup.sh
+++ ubreslin2/moodle/setup.sh
@@ -5,3 +5,5 @@ echo sudo chmod -R 0755 /home/jbreslin/sandbox/moodle
 echo sudo find /home/jbreslin/sandbox/moodle -type f -exec chmod 0644 {} \;
 echo chmod -R "www-data allow read,delete,write,append,file_inherit,directory_inherit" /home/jbreslin/sandbox/moodle
 echo sudo -u postgres psql -f setup.sql
+mkdir /home/jbreslin/sandbox/moodledata
+chmod 0777 /home/jbreslin/sandbox/moodledata
