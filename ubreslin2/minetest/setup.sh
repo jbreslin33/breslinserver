00717c402774c808d7156b967e57594f9f2be92c
jbreslin33 <jbreslin33@localhost>
2015-04-09 10:37:19 -0400

    minetest ready

--- ubreslin2/minetest/setup.sh
+++ ubreslin2/minetest/setup.sh
@@ -8,3 +8,4 @@ git clone https://github.com/minetest/minetest.git
 . cdminetest.sh 
 fi
 cmake . -DENABLE_GETTEXT=1 -DENABLE_FREETYPE=1 -DENABLE_LEVELDB=1 -DENABLE_REDIS=1
+make -j$(grep -c processor /proc/cpuinfo)
