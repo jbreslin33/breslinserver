2670923508908ffc529b07093a310a65aa7c6593
jbreslin33 <jbreslin33@localhost>
2015-04-14 11:13:21 -0400

    log

--- visitationbvm/xfce_mint/setup_minetest.sh
+++ visitationbvm/xfce_mint/setup_minetest.sh
@@ -10,8 +10,10 @@ fi
 
 if [ "$1" = "server" ]; then
 cmake . -DENABLE_GETTEXT=1 -DENABLE_FREETYPE=1 -DENABLE_LEVELDB=1 -DENABLE_REDIS=1 -DBUILD_SERVER=0
+echo server build!!!!!
 else
 cmake . -DENABLE_GETTEXT=1 -DENABLE_FREETYPE=1 -DENABLE_LEVELDB=1 -DENABLE_REDIS=1 -DBUILD_CLIENT=0
+echo client build!!!!!
 fi
 make -j$(grep -c processor /proc/cpuinfo)
 
