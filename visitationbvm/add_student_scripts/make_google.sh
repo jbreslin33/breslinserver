aa7dc697a58b9225757675844189f21d4924e579
jbreslin33 <jbreslin33@localhost>
2015-05-06 11:00:47 -0400

    u

--- visitationbvm/add_student_scripts/make_google.sh
+++ visitationbvm/add_student_scripts/make_google.sh
@@ -2,6 +2,7 @@ cut -d : -f 1,2,5 add_viso.txt > add_school_to_google_A.txt
 sed -i 's/ /:/g' add_school_to_google_A.txt
 sed -i 's/:/,/g' add_school_to_google_A.txt
 cut -d , -f 3,4,1,2 add_school_to_google_A.txt > add_school_to_google_B.txt
+sed '0,/,/s//@visitationbvm.com,google/' add_school_to_google_B.txt > add_school_to_google_C.txt
 sed -i '1s/^/Email Address,Password,First Name, Last Name\n/' add_school_to_google_B.txt 
 
 #echo copy student linux file to a temp file
