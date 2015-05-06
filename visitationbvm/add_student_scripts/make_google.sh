0dbe3037e79ad75af1204697dcaca468712049ab
jbreslin33 <jbreslin33@localhost>
2015-05-06 09:05:07 -0400

    u

--- visitationbvm/add_student_scripts/make_google.sh
+++ visitationbvm/add_student_scripts/make_google.sh
@@ -1,12 +1,14 @@
-echo copy student linux file to a temp file
-cp add_viso.txt add_school_to_moodle.txt
+cut -d : -f 1,2,5 add_viso.txt
 
-echo replace blank space between first and last name with : delimeter
-sed -i 's/ /:/g' add_school_to_moodle.txt
-sed -i 's/:\/bin\/bash//g' add_school_to_moodle.txt
+#echo copy student linux file to a temp file
+#cp add_viso.txt add_school_to_moodle.txt
 
-echo get rid of first two lines k and jbreslin
-sed 1d add_school_to_moodle.txt > add_moodle.txt
-sed 1d add_moodle.txt > add_school_to_moodle.txt
-cp add_school_to_moodle.txt add_moodle.txt
-sed -i '1s/^/username:password:email:institution:firstname:lastname:department\n/' add_moodle.txt 
+#echo replace blank space between first and last name with : delimeter
+#sed -i 's/ /:/g' add_school_to_moodle.txt
+#sed -i 's/:\/bin\/bash//g' add_school_to_moodle.txt
+
+#echo get rid of first two lines k and jbreslin
+#sed 1d add_school_to_moodle.txt > add_moodle.txt
+#sed 1d add_moodle.txt > add_school_to_moodle.txt
+#cp add_school_to_moodle.txt add_moodle.txt
+#sed -i '1s/^/username:password:email:institution:firstname:lastname:department\n/' add_moodle.txt 
