2c02728f40e4ead61e39a0c2a17c74835a858448
jbrelsin33 <jbrelsin33@localhost>
2015-05-06 13:32:13 -0400

    u

--- visitationbvm/add_student_scripts/make_google.sh
+++ visitationbvm/add_student_scripts/make_google.sh
@@ -4,5 +4,6 @@ sed -i 's/:/,/g' add_school_to_google_A.txt
 cut -d , -f 3,4,1,2 add_school_to_google_A.txt > add_school_to_google_B.txt
 sed '/,/s//@visitationbvm.com,google/' add_school_to_google_B.txt > add_school_to_google_C.txt
 sed -i '/New/d' add_school_to_google_C.txt
-sed -i '1s/^/Email Address,Password,First Name, Last Name\n/' add_school_to_google_B.txt 
+sed -i '/kinder/d' add_school_to_google_C.txt
 split -l 100 -d add_school_to_google_C.txt google
+sed -i '1s/^/Email Address,Password,First Name, Last Name\n/' google0* 
