faa82655c19bd31aa7ad25d25f232752461222ca
jbreslin33 <jbreslin33@localhost>
2015-05-06 13:22:55 -0400

    u

--- visitationbvm/add_student_scripts/make_google.sh
+++ visitationbvm/add_student_scripts/make_google.sh
@@ -5,16 +5,4 @@ cut -d , -f 3,4,1,2 add_school_to_google_A.txt > add_school_to_google_B.txt
 sed '/,/s//@visitationbvm.com,google/' add_school_to_google_B.txt > add_school_to_google_C.txt
 sed -i '/New/d' add_school_to_google_C.txt
 sed -i '1s/^/Email Address,Password,First Name, Last Name\n/' add_school_to_google_B.txt 
-
-#echo copy student linux file to a temp file
-#cp add_viso.txt add_school_to_moodle.txt
-
-#echo replace blank space between first and last name with : delimeter
-#sed -i 's/ /:/g' add_school_to_moodle.txt
-#sed -i 's/:\/bin\/bash//g' add_school_to_moodle.txt
-
-#echo get rid of first two lines k and jbreslin
-#sed 1d add_school_to_moodle.txt > add_moodle.txt
-#sed 1d add_moodle.txt > add_school_to_moodle.txt
-#cp add_school_to_moodle.txt add_moodle.txt
-#sed -i '1s/^/username:password:email:institution:firstname:lastname:department\n/' add_moodle.txt 
+split -l 100 -d add_school_to_google_C.txt google
