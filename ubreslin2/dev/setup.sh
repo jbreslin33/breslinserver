0596f3c58c1f226f59dd11f559bdd0942cf8e72d
Jim Breslin <jbreslin33@gmail.com>
2015-06-13 19:26:03 -0400

    dev for git

--- ubreslin2/dev/setup.sh
+++ ubreslin2/dev/setup.sh
@@ -48,54 +48,20 @@ mkdir /home/$1/originals
 sudo cp /etc/apache2/sites-available/000-default.conf /home/$1/originals
 
 if [ "$1" = "jbreslin" ]; then
-hg clone https://jbreslin33@code.google.com/p/breslininput/
-sudo chown -R $1:$2 /home/$1/sandbox/breslininput/
-hg clone https://jbreslin33@code.google.com/p/breslinnetwork/
-sudo chown -R $1:$2 /home/$1/sandbox/breslinnetwork/
-hg clone https://jbreslin33@code.google.com/p/breslintalker/
-sudo chown -R $1:$2 /home/$1/sandbox/breslintalker/
-hg clone https://jbreslin33@code.google.com/p/breslinclient/
-sudo chown -R $1:$2 /home/$1/sandbox/breslinclient/
-hg clone https://jbreslin33@code.google.com/p/breslinmathracer/
+git clone https://github.com/jbreslin33/breslinmathracer.git
 sudo chown -R $1:$2 /home/$1/sandbox/breslinmathracer/
-hg clone https://jbreslin33@code.google.com/p/armygame/
-sudo chown -R $1:$2 /home/$1/sandbox/armygame/
 sudo cp /home/$1/sandbox/breslinserver/ubreslin2/dev/default_jbreslin /etc/apache2/sites-available/000-default.conf
 fi
 
 if [ "$1" = "lbreslin" ]; then
-hg clone https://lbreslin6@code.google.com/p/breslininput/
-sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslininput/
-hg clone https://lbreslin6@code.google.com/p/breslinnetwork/
-sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslinnetwork/
-hg clone https://lbreslin6@code.google.com/p/breslintalker/
-sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslintalker/
-hg clone https://lbreslin6@code.google.com/p/breslinclient/
-sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslinclient/
-hg clone https://lbreslin6@code.google.com/p/breslinmathracer/
+git clone https://github.com/jbreslin33/breslinmathracer.git
 sudo chown -R lbreslin:lbreslin /home/$1/sandbox/breslinmathracer/
-hg clone https://lbreslin6@code.google.com/p/armygame/
-sudo chown -R lbreslin:lbreslin /home/$1/sandbox/armygame/
-hg clone https://lbreslin6@code.google.com/p/lukes-webpage/
-sudo chown -R lbreslin:lbreslin /home/$1/sandbox/lukes-webpage/
 sudo cp /home/$1/sandbox/breslinserver/ubreslin2/dev/default_lbreslin /etc/apache2/sites-available/000-default.conf
 fi
 
 if [ "$1" = "bbreslin" ]; then
-hg clone https://bbreslin99@code.google.com/p/breslininput/
-sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslininput/
-hg clone https://bbreslin99@code.google.com/p/breslinnetwork/
-sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslinnetwork/
-hg clone https://bbreslin99@code.google.com/p/breslintalker/
-sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslintalker/
-hg clone https://bbreslin99@code.google.com/p/breslinclient/
-sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslinclient/
-hg clone https://bbreslin99@code.google.com/p/breslinmathracer/
+git clone https://github.com/jbreslin33/breslinmathracer.git
 sudo chown -R bbreslin:bbreslin /home/$1/sandbox/breslinmathracer/
-hg clone https://bbreslin99@code.google.com/p/armygame/
-sudo chown -R bbreslin:bbreslin /home/$1/sandbox/armygame/
-hg clone https://bbreslin99@code.google.com/p/lukes-webpage/
-sudo chown -R bbreslin:bbreslin /home/$1/sandbox/lukes-webpage/
 sudo cp /home/$1/sandbox/breslinserver/ubreslin2/dev/default_bbreslin /etc/apache2/sites-available/000-default.conf
 fi
 
