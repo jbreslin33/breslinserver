fcc6446b03751935757b2274cd541db2622b1be3
Jim Breslin <jbreslin33@gmail.com>
2015-09-04 10:38:52 -0400

    as

---
+++ visitationbvm/add_student_scripts/make_ttm.sh
@@ -0,0 +1,9 @@
+cut -d : -f 1,2,5 add_viso.txt > add_school_to_google_A.txt
+sed -i 's/ /:/g' add_school_to_google_A.txt
+sed -i 's/:/,/g' add_school_to_google_A.txt
+cut -d , -f 3,4,1,2 add_school_to_google_A.txt > add_school_to_google_B.txt
+sed '/,/s//@visitationbvm.com,google/' add_school_to_google_B.txt > add_school_to_google_C.txt
+sed -i '/New/d' add_school_to_google_C.txt
+sed -i '/kinder/d' add_school_to_google_C.txt
+split -l 100 -d add_school_to_google_C.txt google
+sed -i '1s/^/Email Address,Password,First Name, Last Name\n/' google0* 
