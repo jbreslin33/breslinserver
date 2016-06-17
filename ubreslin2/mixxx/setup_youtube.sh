2544d5abcf070c1d1f88ad29491d65999fe6b2ab
Jim Breslin <jbreslin33@gmail.com>
2016-06-17 13:28:10 -0400

    youtube

--- ubreslin2/mixxx/setup_youtube.sh
+++ ubreslin2/mixxx/setup_youtube.sh
@@ -1,1 +1,13 @@
 sudo apt-get install youtube-dl
+sudo apt-get install ffmpeg
+sudo apt-get install lame
+
+#download youtube video
+#youtube-dl url-of-youtube-vid
+
+#convert youtube to wav 
+#ffmpeg -i song.mkv sound.wav
+
+#convert wav to mp3
+#lame song.wav song.mp3
+
