8b111f9935d4706d36545f205d5b253ea95aec16
Jim Breslin <jbreslin33@gmail.com>
2016-07-06 00:36:49 -0400

    chrome

--- ubreslin2/dev/setup.sh
+++ ubreslin2/dev/setup.sh
@@ -32,7 +32,7 @@ sudo apt-get -y install remmina-plugin-vnc
 
 echo chrome
 sudo apt-get install libxss1 libappindicator1 libindicator7
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 sudo apt-get -f install
 
