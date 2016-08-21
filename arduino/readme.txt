23770587b492cd685565e7cfdb2a2939ae5ce6ae
Jim Breslin <jbreslin33@gmail.com>
2016-08-21 18:16:20 -0400

    readme

---
+++ arduino/readme.txt
@@ -0,0 +1,13 @@
+to run a program:
+
+from code folder e.g. spirit
+ino build 
+ino clean
+ino upload
+
+screen /dev/ttyACM1 9600 //to see serial output
+
+pkill screen //to kill screen or it will get in way of upload
+
+//repeat above steps to rerun
+
--- arduino/serial.sh
+++ arduino/serial.sh
@@ -1,2 +1,2 @@
-stty -F /dev/ttyACM2 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+stty -F /dev/ttyACM1 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
 echo "def" > /dev/ttyACM1
