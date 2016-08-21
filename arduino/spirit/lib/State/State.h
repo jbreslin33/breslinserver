cfa8c7ecba8b3485ab4d8bca1d24e60da7276d07
Jim Breslin <jbreslin33@gmail.com>
2016-08-21 19:22:03 -0400

    sm

--- arduino/spirit/lib/State/State.h
+++ arduino/spirit/lib/State/State.h
@@ -8,14 +8,18 @@
 
 #include "Arduino.h"
 
+template <class entity_type>
 class State
 {
-  public:
-    State(int pin);
-    void dot();
-    void dash();
-  private:
-    int _pin;
+public:
+ 
+  virtual void Enter(entity_type*)=0;
+ 
+  virtual void Execute(entity_type*)=0;
+ 
+  virtual void Exit(entity_type*)=0;
+ 
+  virtual ~State(){}
 };
 
 #endif
