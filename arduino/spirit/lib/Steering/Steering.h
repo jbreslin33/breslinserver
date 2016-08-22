ef2f764b8dbdeb05948468a86b82cf9197188b2c
Jim Breslin <jbreslin33@gmail.com>
2016-08-22 19:18:46 -0400

    moving pin code

--- arduino/spirit/lib/Steering/Steering.h
+++ arduino/spirit/lib/Steering/Steering.h
@@ -5,6 +5,7 @@
 #include "BaseEntity.h"
 #include "SteeringStates.h"
 #include "StateMachine.h"
+#include "Arduino.h"
 
 class Steering : public BaseEntity
 {
@@ -12,8 +13,40 @@ private:
   	StateMachine<Steering>* mStateMachine;
 
 public:
+        //right
+        int rightWheelReverse=8;
+        int rightWheelDrive=11;
+        int rightWheelSpeed=9;
+
+        //left
+        int leftWheelReverse=12;
+        int leftWheelDrive=13;
+        int leftWheelSpeed=10;
+
+        int left_speed = 250;
+        int right_speed = 250;
+        int delayTime = 100;
+
+
+
 Steering()
 {
+        //right
+        pinMode(rightWheelReverse,OUTPUT);
+        pinMode(rightWheelDrive,OUTPUT);
+        pinMode(rightWheelSpeed,OUTPUT);
+
+        //left
+        pinMode(leftWheelReverse,OUTPUT);
+        pinMode(leftWheelDrive,OUTPUT);
+        pinMode(leftWheelSpeed,OUTPUT);
+
+	analogWrite(leftWheelSpeed,left_speed);
+	digitalWrite(leftWheelDrive,LOW);
+     	digitalWrite(leftWheelReverse,LOW);
+
+
+
 	mStateMachine = new StateMachine<Steering>(this);
 	mStateMachine->setGlobalState(SteeringGlobalState::Instance());
 	mStateMachine->setCurrentState(SteeringGlobalState::Instance());
--- arduino/spirit/lib/SteeringStates/SteeringStates.cpp
+++ arduino/spirit/lib/SteeringStates/SteeringStates.cpp
@@ -1,6 +1,5 @@
 #include "SteeringStates.h"
-
-#include "Arduino.h"
+#include "Steering.h"
 
 /*
 GLOBAL
@@ -17,79 +16,19 @@ void SteeringGlobalState::enter(Steering* steering)
 }
 void SteeringGlobalState::execute(Steering* steering)
 {
-	int left_speed = 250;
-	int right_speed = 250;
-	int delayTime = 100;
 
-	//right
-	int rightWheelReverse=8;
-	int rightWheelDrive=11;
-	int rightWheelSpeed=9;
 
-	//left
-	int leftWheelReverse=12;
-	int leftWheelDrive=13;
-	int leftWheelSpeed=10;
 
-        analogWrite(leftWheelSpeed,left_speed);
-       	digitalWrite(leftWheelDrive,HIGH);
-        digitalWrite(leftWheelReverse,LOW);
 
-        analogWrite(rightWheelSpeed,right_speed);
-        digitalWrite(rightWheelDrive,HIGH);
-        digitalWrite(rightWheelReverse,LOW);
+        analogWrite(steering->leftWheelSpeed,steering->left_speed);
+        digitalWrite(steering->leftWheelDrive,HIGH);
+        digitalWrite(steering->leftWheelReverse,LOW);
         
-	delay(delayTime);
-
-        //Serial.print("global_execute");
-
-//speed
-/*
-        if (left < 0)
-        {
-                int speed = left * -1;
-                analogWrite(leftWheelSpeed,speed);
-                digitalWrite(leftWheelDrive,LOW);
-                digitalWrite(leftWheelReverse,HIGH);
-        }
-        if (left == 0)
-        {
-                int speed = 0;
-                analogWrite(leftWheelSpeed,speed);
-                digitalWrite(leftWheelDrive,LOW);
-                digitalWrite(leftWheelReverse,LOW);
-        }
-        if (left > 0)
-        {
-                int speed = left;
-                analogWrite(leftWheelSpeed,speed);
-                digitalWrite(leftWheelDrive,HIGH);
-                digitalWrite(leftWheelReverse,LOW);
-        }
-        if (right < 0)
-        {
-                int speed = right * -1;
-                analogWrite(rightWheelSpeed,speed);
-                digitalWrite(rightWheelDrive,LOW);
-                digitalWrite(rightWheelReverse,HIGH);
-        }
-        if (right == 0)
-        {
-                int speed = 0;
-                analogWrite(rightWheelSpeed,speed);
-                digitalWrite(rightWheelDrive,LOW);
-                digitalWrite(rightWheelReverse,LOW);
-        }
-        if (right > 0)
-        {
-                int speed = right;
-                analogWrite(rightWheelSpeed,speed);
-                digitalWrite(rightWheelDrive,HIGH);
-                digitalWrite(rightWheelReverse,LOW);
-        }
-        delay(delayTime);
-*/
-
+	analogWrite(steering->rightWheelSpeed,steering->left_speed);
+        digitalWrite(steering->rightWheelDrive,HIGH);
+        digitalWrite(steering->rightWheelReverse,LOW);
+       
+	delay(100);
 }
 void SteeringGlobalState::exit(Steering* steering)
 {
