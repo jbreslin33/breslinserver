d55e3847eaa6b68e9cb6b7718167fb0dbef9e041
Jim Breslin <jbreslin33@gmail.com>
2016-08-22 20:44:57 -0400

    beep has sonic code

--- arduino/beep/src/sketch.ino
+++ arduino/beep/src/sketch.ino
@@ -1,101 +1,42 @@
-
-int pinI1=8;//define I1 interface
-int pinI2=11;//define I2 interface 
-int speedpinA=9;//enable motor A
-int pinI3=12;//define I3 interface 
-int pinI4=13;//define I4 interface 
-int speedpinB=10;//enable motor B
-int speed =127;//d
-int speedA = 250; //back left wheel
-int speedB = 250; //back right wheel
-void setup()
-{
-  pinMode(pinI1,OUTPUT);
-  pinMode(pinI2,OUTPUT);
-  pinMode(speedpinA,OUTPUT);
-  pinMode(pinI3,OUTPUT);
-  pinMode(pinI4,OUTPUT);
-  pinMode(speedpinB,OUTPUT);
-}
- 
-void forward()
-{
-     analogWrite(speedpinA,speedA);//input a simulation value to set the speed
-     analogWrite(speedpinB,speedB);
-     
-     digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
-     digitalWrite(pinI3,LOW);
-     
-     digitalWrite(pinI2,HIGH);//turn DC Motor A move anticlockwise
-     digitalWrite(pinI1,LOW);
-}
-void backward()//
-{
-     analogWrite(speedpinA,speedA);//input a simulation value to set the speed
-     analogWrite(speedpinB,speedB);
-     
-     digitalWrite(pinI4,LOW);//turn DC Motor B move anticlockwise
-     digitalWrite(pinI3,HIGH);
-     
-     digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
-     digitalWrite(pinI1,HIGH);
-}
-
-
-
-
-
-void left()//
-{
-     analogWrite(speedpinA,speed);//input a simulation value to set the speed
-     analogWrite(speedpinB,speed);
-     digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
-     digitalWrite(pinI3,LOW);
-     digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
-     digitalWrite(pinI1,LOW);
-}
-void right()//
-{
-     analogWrite(speedpinA,speed);//input a simulation value to set the speed
-     analogWrite(speedpinB,speed);
-     digitalWrite(pinI4,LOW);//turn DC Motor B move anticlockwise
-     digitalWrite(pinI3,HIGH);
-     digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
-     digitalWrite(pinI1,HIGH);
-}
-void stop()//
-{
-     digitalWrite(speedpinA,LOW);// Unenble the pin, to stop the motor. this should be done to avid damaging the motor. 
-     digitalWrite(speedpinB,LOW);
-     delay(1000);
-}
-
-void loop()
-{
- forward(); 
-   delay(1000);
 /*
-   stop();
-    delay(2000);
-  backward();
-   delay(5000);
-    stop();
-     delay(2000);
-     
-     
-  left();
-  delay(2000);
-  
-  stop();
-  right();
-  delay(2000);
-  stop();
-  delay(2000);
-  forward();
-  delay(2000);
-  stop();
-  backward();
-  delay(2000); 
-  stop();
+* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+*
+* Crated by Dejan Nedelkovski,
+* www.HowToMechatronics.com
+*
 */
+
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
 }
