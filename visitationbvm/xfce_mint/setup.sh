25c9b619664427f8f3a3254395e7e102bb62f67f
Jim Breslin <jbreslin33@gmail.com>
2016-10-25 12:00:40 -0400

    chrome

--- visitationbvm/xfce_mint/setup.sh
+++ visitationbvm/xfce_mint/setup.sh
@@ -10,8 +10,8 @@ sudo cp mdm.conf /etc/mdm
 sudo cp newusers /usr/sbin
 
 echo chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
 sudo apt-get install google-chrome-stable
 
