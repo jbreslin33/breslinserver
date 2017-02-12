608ad893e4c903e6a2d761078cf0050af63c0b72
Jim Breslin <jbreslin33@gmail.com>
2017-02-12 16:45:09 -0500

    youtube

--- ubreslin2/mixxx/setup_youtube.sh
+++ ubreslin2/mixxx/setup_youtube.sh
@@ -1,5 +1,9 @@
 sudo apt-get install youtube-dl
 
+sudo apt-get install python-setuptools
+sudo easy_install pip
+sudo pip install --upgrade youtube-dl
+
 sudo add-apt-repository ppa:mc3man/trusty-media
 sudo apt-get update
 sudo apt-get install ffmpeg
