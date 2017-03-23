4d41f26ea73b7bb8b8192392c588fb1c7713922c
Jim Breslin <jbreslin33@gmail.com>
2017-03-23 15:18:30 -0400

    restore minetest db

---
+++ ubreslin2/minetest/restore_db.sh
@@ -0,0 +1,8 @@
+echo
+echo ---------------------------------------------
+echo UBRESLIN: RESTORE MINETEST DB 
+echo ---------------------------------------------
+
+echo restore db with rsync
+rsync -r dotmixxx/ /home/jbreslin/.mixxx/
+
