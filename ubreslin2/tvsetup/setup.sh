a78da9cb64031326f2770446302a013922ab601a
Jim Breslin <jbreslin33@gmail.com>
2017-06-19 12:09:20 -0400

    setup

--- ubreslin2/tvsetup/setup.sh
+++ ubreslin2/tvsetup/setup.sh
@@ -1,2 +1,7 @@
+rm *.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo apt-get update
+sudo apt-get install kodi
