602b6c0526a8f07b06cfdc9c7b0b418ab9761fd3
Jim Breslin <jbreslin33@gmail.com>
2017-07-31 17:38:34 -0400

    setup

--- ubreslin2/dev/setup.sh
+++ ubreslin2/dev/setup.sh
@@ -45,9 +45,9 @@ echo for djing
 sudo apt-get -y install soundconverter
 
 echo virtualbox
-sudo apt-get -y install linux-headers-generic build-essential dkms
-sudo apt-get -y remove --purge virtualbox-dkms
-sudo apt-get -y install virtualbox-dkms
+#sudo apt-get -y install linux-headers-generic build-essential dkms
+#sudo apt-get -y remove --purge virtualbox-dkms
+#sudo apt-get -y install virtualbox-dkms
 
 echo
 echo ---------------------------------------------
