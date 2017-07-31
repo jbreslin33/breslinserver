0211f4ed83d913be43ab464eebdf41eb129977b7
Jim Breslin <jbreslin33@gmail.com>
2017-07-31 14:06:13 -0400

    slack

--- visitationbvm/cinnamon/setup.sh
+++ visitationbvm/cinnamon/setup.sh
@@ -16,6 +16,10 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
 sudo apt-get install google-chrome-stable
+
+echo slack
+sudo dpkg --install slack-desktop-2.6.3-amd64.deb
+
 else
     echo "I'm 32-bit"
 fi
