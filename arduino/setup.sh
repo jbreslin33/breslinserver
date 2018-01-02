fd38424ac059d0a79b0d27dc61df3c7b49e4e9e2
Jim Breslin <jbreslin@gmail.com>
2018-01-01 21:44:44 -0500

    mobdro

--- arduino/setup.sh
+++ arduino/setup.sh
@@ -3,7 +3,7 @@ sudo apt-get install arduino
 sudo apt-get install python-setuptools
 git clone git://github.com/amperka/ino.git
 
-udo apt-get install python-pip
+sudo apt-get install python-pip
 sudo easy_install ino
 
 
--- ubreslin2/tvsetup/setup.sh
+++ ubreslin2/tvsetup/setup.sh
@@ -6,3 +6,10 @@ sudo add-apt-repository -y ppa:team-xbmc/ppa
 sudo apt-get -y update
 sudo apt-get -y install kodi
 sudo apt-get -y install playonlinux
+
+--mobdro
+sudo apt-get -f install
+wget http://static.davidedmundson.co.uk/shashlik/shashlik_0.9.3.deb
+sudo apt-get install libc6-i386
+sudo dpkg -i shashlik_0.9.3.deb 
+
