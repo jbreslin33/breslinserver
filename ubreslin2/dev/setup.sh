0f78a757a63156f29f343ed50c04818759824e66
Jim Breslin <jbreslin33@gmail.com>
2018-02-01 13:34:34 -0500

    vbox

--- ubreslin2/dev/setup.sh
+++ ubreslin2/dev/setup.sh
@@ -66,6 +66,7 @@ echo virtualbox
 #sudo apt-get -y install linux-headers-generic build-essential dkms
 #sudo apt-get -y remove --purge virtualbox-dkms
 #sudo apt-get -y install virtualbox-dkms
+#sudo apt-get -y install virtualbox-qt
 
 echo kvm
 sudo apt-get install qemu-kvm libvirt-bin bridge-utils
