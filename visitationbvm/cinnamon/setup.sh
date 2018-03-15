4fe5c988897dd279f429c62a8ac119e7e9eb0c4d
Jim Breslin <jbreslin33@gmail.com>
2018-03-15 14:48:21 -0400

    hosts

--- visitationbvm/cinnamon/setup.sh
+++ visitationbvm/cinnamon/setup.sh
@@ -6,6 +6,9 @@ sudo apt-get -y upgrade
 
 echo viso specific files
 cp /etc/hosts originalhosts
+echo remove old epop server reference
+sed -i '/192.168.2.105    server/d' ./originalhosts
+echo add new server reference
 sudo echo "192.168.4.10    server" >> originalhosts
 sudo cp originalhosts /etc/hosts
 
