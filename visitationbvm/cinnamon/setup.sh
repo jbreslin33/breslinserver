ede8596ccd341d387a6665d7fda3520d40658605
Jim Breslin <jbreslin33@gmail.com>
2018-03-16 15:15:58 -0400

    build minetest

--- visitationbvm/cinnamon/setup.sh
+++ visitationbvm/cinnamon/setup.sh
@@ -36,6 +36,21 @@ echo install build tools for student coding
 sudo apt-get -y install build-essential 
 sudo apt-get -y install ssh
 
+echo build minetest
+echo minetest dependencies
+sudo apt-get install build-essential cmake git libirrlicht-dev libbz2-dev libgettextpo-dev
+sudo apt-get install libfreetype6-dev libpng12-dev libjpeg8-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev
+sudo apt-get install libogg-dev libvorbis-dev libopenal-dev libhiredis-dev libcurl3-dev
+echo git minetest 
+
+git clone --depth 1 https://github.com/minetest/minetest.git /home/$1/sandbox/minetest
+git clone --depth 1 https://github.com/minetest/minetest_game.git /home/$1/sandbox/minetest/games/minetest_game
+
+echo cmake
+cd /home/$1/sandbox/minetest
+cmake . -DRUN_IN_PLACE=TRUE
+make 
+
 echo install browsers
 sudo apt-get -y install chromium-browser
 
