d9c210a4958f4860518a65e9be26ade3dc7a466e
Jim Breslin <jbreslin33@gmail.com>
2018-03-21 20:23:10 -0400

    ino

--- arduino/setup.sh
+++ arduino/setup.sh
@@ -3,8 +3,8 @@ sudo apt-get install arduino
 sudo apt-get install python-setuptools
 git clone git://github.com/amperka/ino.git
 
-sudo apt-get install python-pip
-sudo easy_install ino
+echo sudo apt-get install python-pip
+echo sudo easy_install ino
 
 
 
