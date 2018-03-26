9fbadcb274e5fca7b918c5ca589f9bf1e5a5e747
Jim Breslin <jbreslin33@gmail.com>
2018-03-26 14:01:47 -0400

    lab 21 26

---
+++ visitationbvm/cluster_scripts/lab_21_26.sh
@@ -0,0 +1,9 @@
+ssh-keygen -f "/home/jbreslin/.ssh/known_hosts" -R r2613
+ssh-keygen -f "/home/jbreslin/.ssh/known_hosts" -R r2614 
+ssh-keygen -f "/home/jbreslin/.ssh/known_hosts" -R r2615
+ssh-keygen -f "/home/jbreslin/.ssh/known_hosts" -R r2616
+ssh-keygen -f "/home/jbreslin/.ssh/known_hosts" -R r2617
+ssh-keygen -f "/home/jbreslin/.ssh/known_hosts" -R r2618
+ssh-keygen -f "/home/jbreslin/.ssh/known_hosts" -R r2619
+ssh-keygen -f "/home/jbreslin/.ssh/known_hosts" -R r2620
+clusterssh student@r2613 student@r2614 student@r2615 student@r2616 student@r2617 student@r2618 student@r2619 student@r2620
