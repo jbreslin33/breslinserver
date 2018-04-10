f7071741a4261f79cbe96812feaf00826e40ccac
Jim Breslin <jbreslin33@gmail.com>
2018-04-10 19:22:03 -0400

    setup mathcore no files

--- ubreslin2/dev/setup_mathcore_no_files.sh
+++ ubreslin2/dev/setup_mathcore_no_files.sh
@@ -1,14 +1,16 @@
+echo -------------------------- checkout mathcore from git ----------------------------
 git clone https://github.com/jbreslin33/breslinmathracer.git /home/$1/sandbox/breslinmathracer
 
+echo ----------------- create mathcore db called jamesanthonybreslin
 sudo -u postgres createdb jamesanthonybreslin
 
-echo -----------------------web server restart------------------------------
+echo ----------------------apache web server restart------------------------------
 sudo service apache2 reload
 sudo /etc/init.d/apache2 restart
 
-echo restore db and update to latest saved
+echo ------------------------restore db and update to latest saved-----------------
 echo from src/ dir run ./src/database/backup/db_restore.sh
 echo from src/ dir run ./src/database/update.sh
 
-echo  backslashpassword postgres dont forget to put a backslash before password, make postgres pass mibesfat
+echo  -------------------------------- set password to mibesfat ----------------
 sudo -u postgres psql postgres
