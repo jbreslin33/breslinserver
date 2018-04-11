8e2c44cda0fc4ea07dc98a1e559afab2d243e4ea
Jim Breslin <jbreslin33@gmail.com>
2018-04-10 20:02:34 -0400

    only copy default.conf

--- ubreslin2/dev/setup_mathcore_files.sh
+++ ubreslin2/dev/setup_mathcore_files.sh
@@ -1,8 +1,8 @@
 echo -----------------------copy files ------------------------------------
 sudo cp default_jbreslin /etc/apache2/sites-available/000-default.conf
-sudo cp apache.conf /etc/phppgadmin
-sudo cp config.inc.php /etc/phppgadmin
-sudo cp postgresql.conf /etc/postgresql/9.5/main
+echo NO sudo cp apache.conf /etc/phppgadmin
+echo NO sudo cp config.inc.php /etc/phppgadmin
+echo NO sudo cp postgresql.conf /etc/postgresql/9.5/main
 
 echo -----------------------web server restart------------------------------
 sudo service apache2 reload
