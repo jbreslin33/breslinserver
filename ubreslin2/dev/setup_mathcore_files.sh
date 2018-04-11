fca37408702727036d6e89bb836d2cb5434fbb1f
Jim Breslin <jbreslin33@gmail.com>
2018-04-10 20:17:45 -0400

    setup files mathcore

--- ubreslin2/dev/setup_mathcore_files.sh
+++ ubreslin2/dev/setup_mathcore_files.sh
@@ -1,6 +1,6 @@
 echo -----------------------copy files ------------------------------------
 sudo cp default_jbreslin /etc/apache2/sites-available/000-default.conf
-echo NO sudo cp apache.conf /etc/phppgadmin
+sudo cp apache.conf /etc/phppgadmin
 echo NO sudo cp config.inc.php /etc/phppgadmin
 echo NO sudo cp postgresql.conf /etc/postgresql/9.5/main
 
