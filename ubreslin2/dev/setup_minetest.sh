1d03e4c9878af73c531418c63b34031049997984
Jim Breslin <jbreslin33@gmail.com>
2018-04-13 19:55:30 -0400

    setup minetest

--- ubreslin2/dev/setup_minetest.sh
+++ ubreslin2/dev/setup_minetest.sh
@@ -1,20 +1,24 @@
-echo
-echo --------------------------------------------
-echo UBRESLIN: UPDATES 
-echo --------------------------------------------
-sudo apt-get -y dist-upgrade
+echo ----------------------update-------------------------
+sudo apt-get -y update        # Fetches the list of available updates
 
-echo build minetest
-echo minetest dependencies
+echo ----------------------upgrade-------------------------
+sudo apt-get -y upgrade       # Strictly upgrades the current packages
+
+echo ----------------------dist upgrade-------------------------
+sudo apt-get -y dist-upgrade  # Installs updates (new ones)
+
+echo ------------------install minetest dependencies -----------------
 sudo apt-get -y install build-essential cmake git libirrlicht-dev libbz2-dev libgettextpo-dev
 sudo apt-get -y install libfreetype6-dev libpng12-dev libjpeg8-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev
 sudo apt-get -y install libogg-dev libvorbis-dev libopenal-dev libhiredis-dev libcurl3-dev
-echo git minetest 
 
-git clone --depth 1 https://github.com/minetest/minetest.git /home/$1/sandbox/minetest
-git clone --depth 1 https://github.com/minetest/minetest_game.git /home/$1/sandbox/minetest/games/minetest_game
+echo ---------------------git minetest ----------------------------------------
+mkdir ~/sandbox
+git clone --depth 1 https://github.com/minetest/minetest.git ~/sandbox/minetest
+git clone --depth 1 https://github.com/minetest/minetest_game.git ~/sandbox/minetest/games/minetest_game
 
-echo cmake
-cd /home/$1/sandbox/minetest
-cmake . -DRUN_IN_PLACE=TRUE
-make 
+echo ---------------------------- build ---------------------
+echo to build
+echo cd ~/sandbox/minetest
+echo cmake . -DRUN_IN_PLACE=TRUE
+echo make 
