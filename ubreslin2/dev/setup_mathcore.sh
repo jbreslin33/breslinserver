c3d152a1afc10739cd31fe99cb3983e7e589d846
Jim Breslin <jbreslin33@gmail.com>
2018-04-19 09:46:30 -0400

    mathcore setup

--- ubreslin2/dev/setup_mathcore.sh
+++ ubreslin2/dev/setup_mathcore.sh
@@ -1,30 +1,25 @@
-git clone https://github.com/jbreslin33/breslinmathracer.git /home/$1/sandbox/breslinmathracer
-sudo cp /home/$1/sandbox/breslinserver/ubreslin2/dev/default_jbreslin /etc/apache2/sites-available/000-default.conf
+echo -------------------------- checkout mathcore from git ----------------------------
+git clone https://github.com/jbreslin33/breslinmathracer.git ~/sandbox/breslinmathracer
 
+echo ----------------- create mathcore db called jamesanthonybreslin
 sudo -u postgres createdb jamesanthonybreslin
 
-echo
-echo ---------------------------------------------
-echo UBRESLIN: COPY FILES
-echo ---------------------------------------------
-sudo cp /home/$1/sandbox/breslinserver/ubreslin2/dev/apache.conf /etc/phppgadmin
-sudo cp /home/$1/sandbox/breslinserver/ubreslin2/dev/config.inc.php /etc/phppgadmin
-sudo cp /home/$1/sandbox/breslinserver/ubreslin2/dev/postgresql.conf /etc/postgresql/9.5/main
+echo ----------------------apache web server restart------------------------------
+sudo service apache2 reload
+sudo /etc/init.d/apache2 restart
 
+echo ------------------------restore db and update to latest saved-----------------
+echo from src/ dir run ./src/database/backup/db_restore.sh
+echo from src/ dir run ./src/database/update.sh
+
+echo  -------------------------------- set password to mibesfat ----------------
+sudo -u postgres psql postgres
+
+echo -----------------------copy files ------------------------------------
+sudo cp default_jbreslin /etc/apache2/sites-available/000-default.conf
+sudo cp apache.conf /etc/phppgadmin
 
 echo -----------------------web server restart------------------------------
 sudo service apache2 reload
 sudo /etc/init.d/apache2 restart
-
-
-echo for fresh
-echo from src/ dir run ./src/database/backup/build.sh
-echo from src/ dir run ./src/database/build_insert.sh
-echo for latest
-echo from src/ dir run ./src/database/backup/db_restore.sh
-echo from src/ dir run ./src/database/update.sh
-
-echo  backslashpassword postgres dont forget to put a backslash before password, make postgres pass mibesfat
-sudo -u postgres psql postgres
-
 
--- ubreslin2/dev/setup_mathcore_files.sh
+++ ubreslin2/dev/setup_mathcore_files.sh
@@ -1,8 +1,6 @@
 echo -----------------------copy files ------------------------------------
 sudo cp default_jbreslin /etc/apache2/sites-available/000-default.conf
 sudo cp apache.conf /etc/phppgadmin
-echo NO sudo cp config.inc.php /etc/phppgadmin
-echo NO sudo cp postgresql.conf /etc/postgresql/9.5/main
 
 echo -----------------------web server restart------------------------------
 sudo service apache2 reload
