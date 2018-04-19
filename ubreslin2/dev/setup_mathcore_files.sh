1aebac8f5a06f7b24060a72f88a6b63510076b8f
Jim Breslin <jbreslin33@gmail.com>
2018-04-19 09:47:04 -0400

    rm mathcore file

--- ubreslin2/dev/setup_mathcore_files.sh
+++
@@ -1,7 +0,0 @@
-echo -----------------------copy files ------------------------------------
-sudo cp default_jbreslin /etc/apache2/sites-available/000-default.conf
-sudo cp apache.conf /etc/phppgadmin
-
-echo -----------------------web server restart------------------------------
-sudo service apache2 reload
-sudo /etc/init.d/apache2 restart
