8378420164b2bdfb6457df14acf080440bf2c71c
Jim Breslin <jbreslin33@gmail.com>
2018-07-13 13:07:24 -0400

    wine

--- ubreslin2/dev/setup_wine.sh
+++ ubreslin2/dev/setup_wine.sh
@@ -1,6 +1,4 @@
-echo virtualbox
-sudo apt-get -y install linux-headers-generic build-essential dkms
-sudo apt-get -y remove --purge virtualbox-dkms
-sudo apt-get -y install virtualbox-dkms
-sudo apt-get -y install virtualbox-qt
-
+echo wine
+sudo add-apt-repository ppa:ubuntu-wine/ppa
+sudo apt-get update
+sudo apt-get install wine1.8 winetricks
