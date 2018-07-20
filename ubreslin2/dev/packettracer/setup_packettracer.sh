18b3f9b4a8e2dbd75727fb5c93e0dcea8fbed34e
Jim Breslin <jbreslin33@gmail.com>
2018-07-20 10:32:48 -0400

    pt

--- ubreslin2/dev/packettracer/setup_packettracer.sh
+++ ubreslin2/dev/packettracer/setup_packettracer.sh
@@ -1,2 +1,7 @@
-mkdir PacketTracer711
-sudo tar -xvf Packet-Tracer-7.1.1-for-Linux-64-bit.tar -C PacketTracer711/
+#mkdir PacketTracer711
+#sudo tar -xvf Packet-Tracer-7.1.1-for-Linux-64-bit.tar -C PacketTracer711/
+#cd PacketTracer711
+#sudo ./install
+sudo apt-get install qtmultimedia5-dev
+sudo apt-get install libqt5script5 
+sudo apt-get install libqt5scripttools5 
