06e37c36952604c2f0303792d8e3293b2caefbb5
Jim Breslin <jbreslin33@gmail.com>
2018-07-26 10:36:20 -0400

    not copying everything from .mixxx maybe this will stop weird mouse behavour. only copying db and controller and anyalysis

--- ubreslin2/dev/setup_mixxx.sh
+++ ubreslin2/dev/setup_mixxx.sh
@@ -1,8 +1,18 @@
 echo -------------------------- clone from git ----------------------------
-git clone https://github.com/godotengine/godot.git ~/sandbox/godot
+git clone https://github.com/mixxxdj/mixxx.git ~/sandbox/mixxx
+
+echo ------------------- add to sources.list  -----------
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 886DDD89
 
 echo -------------------------- install build tools ----------------------------
-sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libfreetype6-dev libssl-dev libudev-dev libxi-dev libxrandr-dev 
+sudo apt-get install g++ git scons libportmidi-dev libopusfile-dev \
+  libshout-dev libtag1-dev libprotobuf-dev protobuf-compiler \
+  libusb-1.0-0-dev libfftw3-dev libmad0-dev portaudio19-dev \
+  libchromaprint-dev librubberband-dev libsqlite3-dev \
+  libid3tag0-dev libflac-dev libsndfile-dev libupower-glib-dev \
+  libavcodec-dev libavformat-dev libgl-dev liblilv-dev \
+  libjack-dev libjack0 portaudio19-dev \  # because of Bug #1464120
+  libfaad-dev libmp4v2-dev \  # required for M4A support
 
 echo to compile goto godot root and run : scons platform=x11
 
