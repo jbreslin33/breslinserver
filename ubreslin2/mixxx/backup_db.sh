a09dadb6882a3edb58c03de0e4dedecf3509e602
Jim Breslin <jbreslin33@gmail.com>
2018-07-28 18:18:48 -0400

    new stuff is old

--- ubreslin2/mixxx/backup_db.sh
+++ ubreslin2/mixxx/backup_db.sh
@@ -4,10 +4,4 @@ echo UBRESLIN: BACKUP MIXXX DB
 echo ---------------------------------------------
 
 echo backup db with rsync
-#rsync -r ../../../.././.mixxx/ dotmixxx/
-rsync -r ../../../.././.mixxx/analysis dotmixxx/analysis
-rsync ../../../.././.mixxx/mixxxdb.sqlite dotmixxx/mixxxdb.sqlite
-
-#rsync ../../../.././.mixxx/controllers/Numark-Mixtrack-3.midi.xml dotmixxx/controllers/Numark-Mixtrack-3.midi.xml
-#rsync ../../../.././.mixxx/controllers/Numark-Mixtrack-3-scripts.js dotmixxx/controllers/Numark-Mixtrack-3-scripts.js
-
+rsync -r ../../../.././.mixxx/ dotmixxx/
--- ubreslin2/mixxx/restore_db.sh
+++ ubreslin2/mixxx/restore_db.sh
@@ -7,12 +7,9 @@ echo create music directory
 sudo mkdir /amusic
 sudo chmod -R 777 /amusic
 
-echo symbolic link to external drive
-sudo ln -s /media/jbreslin/A01677801677566E/amusic/* /amusic
-
 echo restore db with rsync
-#rsync -r dotmixxx/ ../../../.././.mixxx/
-rsync -r dotmixxx/analysis ../../../.././.mixxx/analysis 
-rsync dotmixxx/mixxxdb.sqlite ../../../.././.mixxx/mixxxdb.sqlite
-#rsync dotmixxx/controllers/Numark-Mixtrack-3.midi.xml ../../../.././.mixxx/controllers/Numark-Mixtrack-3.midi.xml
-#rsync dotmixxx/controllers/Numark-Mixtrack-3-scripts.js ../../../.././.mixxx/controllers/Numark-Mixtrack-3-scripts.js 
+rsync -r dotmixxx/ ../../../.././.mixxx/
+
+echo set symbolic link from music folder to /amusic
+
+
