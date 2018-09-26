2e35ea1391d22f22590af2ff8a3faf78d0e7262f
Jim Breslin <jbreslin33@gmail.com>
2018-09-26 19:05:30 -0400

    java

--- ubreslin2/dev/setup_java.sh
+++ ubreslin2/dev/setup_java.sh
@@ -1,5 +1,7 @@
 echo install java!!!!!
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get -y update
-sudo apt-get -y install oracle-java8-installer
+#sudo add-apt-repository -y ppa:webupd8team/java
+#sudo apt-get -y update
+#sudo apt-get -y install oracle-java8-installer
+sudo apt-get install openjdk-11-jdk-headless
+
 
