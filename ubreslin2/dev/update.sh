04c6a16fa98be3760e12adb977214ce19e116849
Jim Breslin <jbreslin33@gmail.com>
2019-02-07 11:56:59 -0500

    af

---
+++ ubreslin2/dev/update.sh
@@ -0,0 +1,9 @@
+echo ----------------------update-------------------------
+sudo apt-get -y update        # Fetches the list of available updates
+
+echo ----------------------upgrade-------------------------
+sudo apt-get -y upgrade       # Strictly upgrades the current packages
+
+echo ----------------------dist upgrade-------------------------
+sudo apt-get -y dist-upgrade  # Installs updates (new ones)
+
