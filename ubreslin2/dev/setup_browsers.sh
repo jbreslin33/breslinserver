a3454cca933cf63735b903ca36b3b282745647c2
Jim Breslin <jbreslin33@gmail.com>
2019-02-14 13:03:55 -0500

    browswers

--- ubreslin2/dev/setup_browsers.sh
+++ ubreslin2/dev/setup_browsers.sh
@@ -2,19 +2,21 @@ var_chrome=$(which google-chrome)
 echo "$var_chrome";
 
 if [ `getconf LONG_BIT` = "64" ]
+
 then
-    echo "I'm 64-bit"
-echo chrome
+	echo "I'm 64-bit"
+	echo chrome
 
         if [ "$var_chrome" = "/usr/bin/google-chrome" ]
-        then
+        	then
                 echo "google-chrome installed already"
         else
-                wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-                sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-                sudo apt-get -y update
-                sudo apt-get -y install google-chrome-stable
-        fi
+                echo "google-chrome not installed - install it..."
+		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+		sudo gdebi google-chrome-stable_current_amd64.deb
+		rm google-chrome-stable_current_amd64.deb
+fi
+
 else
     echo "I'm 32-bit so I cannot install google-chrome"
 fi
