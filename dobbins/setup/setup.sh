c817b75a3a5630531bcca8ee6004db812cc2a0f3
Jim Breslin <jbreslin33@gmail.com>
2019-10-24 10:29:50 -0400

    changed dobbins setup to run all scripts needed

--- dobbins/setup/setup.sh
+++ dobbins/setup/setup.sh
@@ -1,3 +1,9 @@
+echo run regular setup
+/home/student/sandbox/breslinserver/ubreslin2/dev/setup.sh
+
+echo run browser setup
+/home/student/sandbox/breslinserver/ubreslin2/dev/setup_browsers.sh
+
 echo fix lightdm login 
 sudo cp 50-unity-greeter.conf /usr/share/lightdm/lightdm.conf.d/
 
