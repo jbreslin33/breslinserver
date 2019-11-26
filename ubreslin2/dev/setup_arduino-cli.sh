8a416b8418364fc0202ffee4f5961554010ce248
Jim Breslin <jbreslin33@gmail.com>
2019-11-26 11:52:24 -0500

    setup arduino-cli script

--- ubreslin2/dev/setup_arduino-cli.sh
+++ ubreslin2/dev/setup_arduino-cli.sh
@@ -1,3 +1,11 @@
+echo for instructions goto: https://github.com/arduino/arduino-cli
+echo rm old arduino-cli from /usr/local/bin
+sudo rm /usr/local/bin/arduino-cli
+echo curl to dowload
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
-echo for instrucations goto: https://github.com/arduino/arduino-cli
+echo mv executable to /usr/local/bin
+sudo mv bin/arduino-cli /usr/local/bin
+echo rm original
+sudo rm -r bin
+
 
