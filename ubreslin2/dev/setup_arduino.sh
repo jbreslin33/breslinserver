e28ccf69ab8296402f61f928c185480d8cb58667
Jim Breslin <jbreslin33@gmail.com>
2019-11-27 07:51:19 -0500

    setup arduino

--- ubreslin2/dev/setup_arduino.sh
+++ ubreslin2/dev/setup_arduino.sh
@@ -3,4 +3,7 @@ git clone --depth 1 https://github.com/arduino/Arduino.git ~/sandbox/arduino
 cd ~/sandbox/arduino/build
 ant dist
 ant run
+cd ~/sandbox/arduino/build/linux/work
+echo install arduino system wide
+./install.sh
 
