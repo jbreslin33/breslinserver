9f160590a45fefba1daf33be5c0cb587d49e6ffd
Jim Breslin <jbreslin33@gmail.com>
2019-12-05 08:15:39 -0500

    took out ant run from setup arduino so IDE does not load up

--- ubreslin2/dev/setup_arduino.sh
+++ ubreslin2/dev/setup_arduino.sh
@@ -2,7 +2,7 @@ sudo apt-get install git make gcc ant openjdk-8-jdk unzip
 git clone --depth 1 https://github.com/arduino/Arduino.git ~/sandbox/arduino
 cd ~/sandbox/arduino/build
 ant dist
-ant run
+echo ant run
 cd ~/sandbox/arduino/build/linux/work
 echo install arduino system wide
 sudo ./install.sh
