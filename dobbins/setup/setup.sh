851c9a77152c655e475bf5c3a23f0d4471dfcf39
tahj <7770662@philasd.org>
2019-12-09 14:27:13 -0500

    added newbs to root

--- dobbins/setup/setup.sh
+++ dobbins/setup/setup.sh
@@ -16,16 +16,36 @@ sudo newusers ../add_student_scripts/add_dobbins.txt
 sudo apt-get upgrade
 
 echo add users to root
+sudo usermod -aG sudo abrown
+sudo usermod -aG sudo aknuckles
+sudo usermod -aG sudo arankin
+sudo usermod -aG sudo bmay
+sudo usermod -aG sudo cwilliams
+sudo usermod -aG sudo dwooten
+sudo usermod -aG sudo efulton 
+sudo usermod -aG sudo jbreslin
+sudo usermod -aG sudo jhughes
+sudo usermod -aG sudo jlong
+sudo usermod -aG sudo jross
+sudo usermod -aG sudo jwarfield
+sudo usermod -aG sudo kholmes
 sudo usermod -aG sudo kkeels
 sudo usermod -aG sudo kwright
+sudo usermod -aG sudo lgates
+sudo usermod -aG sudo lstanton
+sudo usermod -aG sudo mward
+sudo usermod -aG sudo nhollowman
+sudo usermod -aG sudo nmurray
+sudo usermod -aG sudo nthomas
+sudo usermod -aG sudo nthrones
+sudo usermod -aG sudo sdouglas
 sudo usermod -aG sudo sdouglass
-sudo usermod -aG sudo efulton
-sudo usermod -aG sudo jbreslin
-sudo usermod -aG sudo lgates 
-sudo usermod -aG sudo smyrick  
-sudo usermod -aG sudo sjones  
-sudo usermod -aG sudo dwooten  
-
-echo complete you can now reboot and login 
+sudo usermod -aG sudo sjones
+sudo usermod -aG sudo smyrick
+sudo usermod -aG sudo sshields
+sudo usermod -aG sudo tepps
+sudo usermod -aG sudo tfleming
+sudo usermod -aG sudo ttribble
+sudo usermod -aG sudo wwhitaker
 
 
