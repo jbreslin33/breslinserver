c9aa333eba23d71d2ab301eba79599c0ff6e3bbf
Jim Breslin <jbreslin33@gmail.com>
2020-01-30 09:37:03 -0500

    youtube

--- ubreslin2/mixxx/setup_youtube.sh
+++ ubreslin2/mixxx/setup_youtube.sh
@@ -16,19 +16,3 @@ sudo apt-get install curl -y
 curl -L https://yt-dl.org/latest/youtube-dl -o /usr/bin/youtube-dl
 sudo curl -L https://yt-dl.org/latest/youtube-dl -o /usr/bin/youtube-dl
 sudo chmod 755 /usr/bin/youtube-dl
-#youtube-dl https://www.youtube.com/watch?v=xpVfcZ0ZcFM
-
-#download youtube video
-#youtube-dl url-of-youtube-vid
-
-#convert youtube to wav 
-#ffmpeg -i song.mkv sound.wav
-
-#convert wav to mp3
-#lame song.wav song.mp3
-
-#dowload youtube playlist
-#youtube-dl -i --extract-audio --audio-format mp3 https://www.youtube.com/playlist?list=PLWSWrBqgaJFMe8kA1WLHI9XrggJrZHGX1 
-
-#preserver playlist order 
-#youtube-dl -i --extract-audio --audio-format mp3 -o "%(playlist_index)s-%(title)s.%(ext)s" https://www.youtube.com/playlist?list=PLWSWrBqgaJFMe8kA1WLHI9XrggJrZHGX1 
