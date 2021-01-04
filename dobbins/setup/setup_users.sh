6dec6d9e40ce09101c14c09b81e6b92de73004ea
Jim Breslin <jbreslin33@gmail.com>
2021-01-04 10:32:06 -0500

    dobbins users

---
+++ dobbins/setup/setup_users.sh
@@ -0,0 +1,30 @@
+echo add new users
+sudo newusers ../add_student_scripts/add_dobbins.txt
+
+echo add users to root
+sudo usermod -aG sudo abrown
+sudo usermod -aG sudo aknuckles
+sudo usermod -aG sudo arankin
+sudo usermod -aG sudo bmay
+sudo usermod -aG sudo cwilliams
+sudo usermod -aG sudo dwooten
+sudo usermod -aG sudo efulton
+sudo usermod -aG sudo jhughes
+sudo usermod -aG sudo jlong
+sudo usermod -aG sudo jross
+sudo usermod -aG sudo jwarfield
+sudo usermod -aG sudo kkeels
+sudo usermod -aG sudo kwright
+sudo usermod -aG sudo lgates
+sudo usermod -aG sudo mward
+sudo usermod -aG sudo nhollowman
+sudo usermod -aG sudo nmurray
+sudo usermod -aG sudo nthrones
+sudo usermod -aG sudo sdouglass
+sudo usermod -aG sudo sjones
+sudo usermod -aG sudo smyrick
+sudo usermod -aG sudo tepps
+sudo usermod -aG sudo tfleming
+sudo usermod -aG sudo ttribble
+sudo usermod -aG sudo wwhitaker
+
