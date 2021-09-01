d3593b7ce685f7b0b16b8996e9ee78c85b90b67e
Jim Breslin <jbreslin33@gmail.com>
2021-09-01 10:26:47 -0400

    penrose

---
+++ penrose/setup/setup.sh
@@ -0,0 +1,8 @@
+echo run regular setup
+/home/student/sandbox/breslinserver/ubreslin2/dev/setup.sh
+
+echo run browser setup
+/home/student/sandbox/breslinserver/ubreslin2/dev/setup_browsers.sh
+
+sudo apt-get upgrade
+
