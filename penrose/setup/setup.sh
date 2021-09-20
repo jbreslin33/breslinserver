5c75a042c3e22fc20740cd675910d2dd6284d623
Jim Breslin <jbreslin33@gmail.com>
2021-09-20 10:05:43 -0400

    tuxpaint

--- penrose/setup/setup.sh
+++ penrose/setup/setup.sh
@@ -4,5 +4,10 @@ echo run regular setup
 echo run browser setup
 /home/student/sandbox/breslinserver/ubreslin2/dev/setup_browsers.sh
 
+echo games
+sudo apt-get -y install minetest
+sudo apt-get -y install tuxtype
+sudo apt-get -y install tuxpaint
+
 sudo apt-get upgrade
 
