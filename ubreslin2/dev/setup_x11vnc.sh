2096d4f090cdb23635888905393769f00309b47e
Jim Breslin <jbreslin33@gmail.com>
2023-11-26 23:16:26 -0500

    x11vnc

---
+++ ubreslin2/dev/setup_x11vnc.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install x11vnc 
+x11vnc -storepasswd 
+x11vnc -usepw -display :0 
+echo further directions: https://tecadmin.net/setup-x11vnc-server-on-ubuntu-linuxmint/
+
