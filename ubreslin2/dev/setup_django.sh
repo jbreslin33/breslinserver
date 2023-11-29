427f6c62cac5e1ff559f5ee03efbbdd742cb6a5f
Jim Breslin <jbreslin33@gmail.com>
2023-11-29 18:48:41 -0500

    setup django

---
+++ ubreslin2/dev/setup_django.sh
@@ -0,0 +1,8 @@
+echo sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade
+
+echo sudo apt-get install python3-pip
+sudo apt-get install python3-pip
+
+echo python3.10 -m pip install Django
+python3.10 -m pip install Django
